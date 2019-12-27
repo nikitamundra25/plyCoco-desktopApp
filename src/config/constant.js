bfa03aed1cc2cdcc0172a4e362f9b53259b45ae2
bhumikabalchandani8 <bhumika.chapter247@gmail.com>
2019-12-27 18:37:20 +0530

    Added Billing settings, Personal Information and Qualification Attribute component

--- src/config/constant.js
+++ src/config/constant.js
@@ -51,3 +51,65 @@ export const LegalForm =[
   { value: "Ltd.", label: "Ltd." },
   { value: "GbR", label: "GbR" }
 ]
+
+export const QualificationAttributes =[
+  { value: "careGiver", label: "Care giver" },
+  { value: "anesthesiaAssistance", label: "Anesthesia Assistance" },
+  { value: "doctorAttentionCommission", label: "Doctor Attention commission" },
+  { value: "medicalAssistance", label: "Medical Assistance" },
+  { value: "Dialysis", label: "Dialysis" },
+  { value: "Individual", label: "Individual" },
+  { value: "Endoscopy", label: "Endoscopy" },
+  { value: "Obstetrics", label: "Obstetrics" },
+  { value: "HKP", label: "HKP (Home nursing)" },
+  { value: "Midwife/Delivery", label: "Midwife/Delivery" },
+  { value: "Heilerizehungspfleger", label: "Heilerizehungspfleger" },
+  { value: "HomeVentilationAll", label: "Home ventilation all" },
+  { value: "HomeVentilationKS", label: "Home ventilation KS" },
+  { value: "HomeManagement", label: "Home Management" },
+  { value: "CardiacCatheterizationLaboratory", label: "Cardiac catheterization laboratory" },
+  { value: "Hygiene", label: "Hygiene" },
+  { value: "Intensive", label: "Intensive" },
+  { value: "IntermediateCare", label: "IMC (Intermediate care)" },
+  { value: "ChildrenIntensive", label: "Children intensive" },
+  { value: "Nurse", label: "Nurse/carer" },
+  { value: "MTRA", label: "MTRA (Medical – technical radiology assistant)" },
+  { value: "Neonatology", label: "Neonatology" },
+  { value: "OP", label: "OP" },
+  { value: "OTA", label: "OTA" },
+  { value: "PalliativeCare", label: "Palliative care" },
+  { value: "PDL", label: "PDL" },
+  { value: "NursingAssistant", label: "Nursing assistant (Former household manager)" },
+  { value: "Physiotherapist", label: "Physiotherapist" },
+  { value: "Psychiatry", label: "Psychiatry" },
+  { value: "Paramedic", label: "Paramedic" },
+  { value: "EmergencyDepartment", label: "Emergency department" },
+  { value: "Wohnbereichsleitung", label: "Wohnbereichsleitung" },
+  { value: "WoundManagement", label: "Wound Management" }
+
+]
+export const Hours =[
+  { value: "12", label: "12" },
+  { value: "1", label: "1" },
+  { value: "2", label: "2" },
+  { value: "3", label: "3" },
+  { value: "4", label: "4" }
+]
+// export const BankAccount =[
+//   { value: "DeutscheBank", label: "Deutsche Bank" },
+//   { value: "Commerzbank", label: "Commerzbank" },
+//   { value: "KFWBankgruppe", label: "KFW Bankgruppe" },
+//   { value: "DZBank", label: "DZ Bank" },
+//   { value: "NRWBank", label: "NRW Bank" }
+// ]
+
+export const CalculationInterval =[
+  { value: "MonthlyTo1", label: "Monthly to 1" },
+  { value: "Bi-monthlyToThe1&16", label: "Bi-monthly to the 1 & 16" },
+  { value: "WeeklyOnMondays", label: "Weekly on Mondays" },
+  
+]
+export const Supplements =[
+  { value: "Exclusive", label: "Exclusive" },
+  { value: "Cumulative", label: "Cumulative" },  
+]

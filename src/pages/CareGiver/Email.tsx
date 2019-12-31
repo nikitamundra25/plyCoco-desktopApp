ff5e9766f957fbfd4c07721542583ec2d822fc2d
rohit-chapter <rohit.chapter247@gmail.com>
2019-12-31 15:16:09 +0530

    email module pages generate

--- src/pages/CareGiver/Email.tsx
+++ src/pages/CareGiver/Email.tsx
@@ -1,7 +1,47 @@
 import React, { Component } from "react";
+import { Button, Col, Row, Form, Nav, NavItem, NavLink } from "reactstrap";
 class Email extends Component {
   render() {
-    return <h5>Email</h5>;
+    return (
+      <div className="email-section">
+        <Nav tabs className="custom-tabs">
+          <NavItem>
+            <NavLink className="active">
+              <span className="icon">
+                <i className="fa fa-inbox"></i>
+              </span>
+              <span>Inbox</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="">
+              <span className="icon">
+                <i className="fa fa-send"></i>
+              </span>
+              <span>Sent Email</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="">
+              <span className="icon">
+                <i className="fa fa-edit"></i>
+              </span>
+              <span>New Email</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="">
+              <span className="icon">
+                <i className="fa fa-cogs"></i>
+              </span>
+              <span>Settings</span>
+            </NavLink>
+          </NavItem>
+        </Nav>
+
+        <div className="email-content"></div>
+      </div>
+    );
   }
 }
 export default Email;
---
+++ src/pages/CareGiver/EmailSettings.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Button, Col, Row, Form, Nav, NavItem, NavLink } from "reactstrap";
+class Email extends Component {
+  render() {
+    return <div className="email-content">Email Settings</div>;
+  }
+}
+export default Email;

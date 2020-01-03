7db53b2d32a7c46bbf220c5357a97fefcbe7dbd7
sohail <sohail.chapter247@gmail.com>
2020-01-03 20:07:54 +0530

    design changes

--- src/pages/CareGiver/Offer.tsx
+++ src/pages/CareGiver/Offer.tsx
@@ -1,7 +1,88 @@
 import React, { Component } from "react";
+import {
+  Button,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Row,
+  Form,
+  CustomInput,
+  Card,
+  InputGroup,
+  InputGroupAddon
+} from "reactstrap";
 class Offer extends Component {
   render() {
-    return <h5>Offer</h5>;
+    return <div className="offer-section">
+     <h5 className="content-title">Offsers</h5>
+      <Row className="custom-col">
+        <Col md={4}>
+          <Card>
+          <div className="offer-wrap">
+          <h5 className="heading toggle-filter">
+            Offer for <span className="font-weight-bold">(Positive)</span>
+          </h5>
+          <div className="offer-content-wrap">
+              <ul>
+                <li className="ative">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="pending">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="pending">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="ative">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="dective">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="active">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="active">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="active">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="dective">Lorem Ipsum is simply dummy text of the printing </li>
+              </ul>
+          </div>
+          </div>
+      </Card>
+        </Col>
+        <Col md={4}>
+          <Card>
+          <div className="offer-wrap">
+          <h5 className="heading toggle-filter">
+            No offer for <span className="font-weight-bold">(Positive)</span>
+          </h5>
+          <div className="offer-content-wrap">
+              <ul>
+                <li className="ative">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="pending">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="pending">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="ative">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="dective">Lorem Ipsum is simply dummy text of the printing </li>
+               
+              </ul>
+          </div>
+          </div>
+      </Card>
+        </Col>
+        <Col md={4}>
+          <Card>
+          <div className="offer-wrap">
+          <h5 className="heading toggle-filter">
+            Worked at <span className="font-weight-bold">(Positive)</span>
+          </h5>
+          <div className="offer-content-wrap">
+              <ul>
+                <li className="ative">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="pending">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="pending">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="ative">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="dective">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="active">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="active">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="active">Lorem Ipsum is simply dummy text of the printing </li>
+                <li className="dective">Lorem Ipsum is simply dummy text of the printing </li>
+              </ul>
+          </div>
+          </div>
+      </Card>
+        </Col>
+      </Row>
+    </div>
+      ;
   }
 }
 export default Offer;

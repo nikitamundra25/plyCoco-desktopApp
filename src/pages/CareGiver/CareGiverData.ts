eb81726606bd8df8a97668421cf25fbda416da7a
bhumikabalchandani8 <bhumika.chapter247@gmail.com>
2020-01-06 19:10:59 +0530

    added json of caregiver, region and employee

---
+++ src/pages/CareGiver/CareGiverData.ts
@@ -0,0 +1,43 @@
+export const userData = [
+    {
+        name: "Sir John Doe",
+        email: "john@gmail.com",
+        phone: "+49 564575678",
+        qualification: ["Care", "Dialysis"],
+        region: ["North Germany", "Cologne", " Frankfurt"],
+        applyingAs: "Individual"
+    },
+    {
+        name: "Sir Martin Lee",
+        email: "martinlee@gmail.com",
+        phone: "+49 564575678",
+        qualification: ["Intensive", "Anesthesia Assistance"],
+        region: ["Central Germany", "Cologne", " Frankfurt"],
+        applyingAs: "UG"
+    },
+    {
+        name: "Sir Tim Cooper",
+        email: "coopert@gmail.com",
+        phone: "+49 564575678",
+        qualification: ["OTA", "Paramedic", "Obstetrics"],
+        region: ["North Germany", "Cologne", " Frankfurt"],
+        applyingAs: "GmbH"
+    },
+    {
+        name: "Sir Ritchie Hudson",
+        email: "hudson@gmail.com",
+        phone: "+49 564575678",
+        qualification: ["Nursing assistant (Former household manager)"],
+        region: ["Cologne", "Munich", " Frankfurt"],
+        applyingAs: "Ltd."
+    },
+    {
+        name: "Sir Mark Potter",
+        email: "mark@gmail.com",
+        phone: "+49 564575678",
+        qualification: ["Heilerizehungspfleger", "Children intensive"],
+        region: ["Central Germany", " Frankfurt"],
+        applyingAs: "Gbr"
+    }
+
+]

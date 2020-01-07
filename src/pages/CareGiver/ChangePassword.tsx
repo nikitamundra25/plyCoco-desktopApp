50869626dc4bee9131b9f3ec74a0309b5ac8a9ab
rohit-chapter <rohit.chapter247@gmail.com>
2020-01-07 12:05:49 +0530

    password page design

--- src/pages/CareGiver/ChangePassword.tsx
+++ src/pages/CareGiver/ChangePassword.tsx
@@ -7,45 +7,37 @@ class ChangePassword extends Component {
       <div>
         <Form className="form-section">
           <Row>
-            <Col lg={"6"}>
+            <Col lg={"12"}>
               <h5 className="main-title mb-4">Change Password</h5>
               <div className="form-card">
                 <Row>
                   <Col lg={"12"}>
                     <FormGroup>
                       <Row>
-                        <Col sm="5">
-                          <Label className="form-label col-form-label ">
-                            Old Password<span className="required">*</span>
-                          </Label>
-                        </Col>
-                        <Col sm="7">
-                          <div>
-                            <Input
-                              type="password"
-                              name={"OldPassword"}
-                              placeholder=" Old Password"
-                            />
-                          </div>
-                        </Col>
-                      </Row>
-                    </FormGroup>
-                  </Col>
-
-                  <Col lg={"12"}>
-                    <FormGroup>
-                      <Row>
-                        <Col sm="5">
+                        <Col sm="3">
                           <Label className="form-label col-form-label ">
                             New Password<span className="required">*</span>
                           </Label>
                         </Col>
-                        <Col sm="7">
+                        <Col sm="5">
                           <Input
-                            type="password"
+                            type="text"
+                            value="Gvbzad436#d"
                             name={"Newpassword"}
-                            placeholder=" New Password"
+                            placeholder="New Password"
                           />
+                        </Col>
+                        <Col sm="4">
+                          <Button
+                            color="primary"
+                            type="submit"
+                            className="password-btn1 d-flex align-items-center justify-content-center"
+                          >
+                            <i className="fa fa-key mr-2"></i>{" "}
+                            <span className="align-middle">
+                              Generate Other Password
+                            </span>
+                          </Button>
                         </Col>
                       </Row>
                     </FormGroup>
@@ -54,36 +46,24 @@ class ChangePassword extends Component {
                   <Col lg={"12"}>
                     <FormGroup>
                       <Row>
-                        <Col sm="5">
-                          <Label className="form-label col-form-label ">
-                            Confirm New Password
-                            <span className="required">*</span>
-                          </Label>
-                        </Col>
-                        <Col sm="7">
-                          <Input
-                            type="password"
-                            name={"ConfirmNewpassword"}
-                            placeholder="Confirm New Password"
-                          />
+                        <Col sm="3"></Col>
+                        <Col sm="5"></Col>
+                        <Col sm="4">
+                          <Button
+                            color="primary"
+                            type="submit"
+                            className="password-btn2 d-flex align-items-center justify-content-center"
+                          >
+                            <i className="fa fa-envelope-o mr-2"></i>{" "}
+                            <span className="align-middle">
+                              Change and Send Email
+                            </span>
+                          </Button>
                         </Col>
                       </Row>
                     </FormGroup>
                   </Col>
                 </Row>
-              </div>
-              <div className="message-text">
-                The new password must be between 6 and 255 characters long.
-              </div>
-            </Col>
-            <Col lg={"12"}>
-              <div className="d-flex align-items-center justify-content-between">
-                <div className="mandatory-text">* Required Fields</div>
-                <div className={"text-right"}>
-                  <Button color="primary" type="submit" className="btn-sumbit">
-                    Change Password
-                  </Button>
-                </div>
               </div>
             </Col>
           </Row>

9ed1b52a6de1d2216966e125b5be9a020d9bdbee
riyagupta247 <riya.chapter247@gmail.com>
2020-01-08 20:10:31 +0530

    add caregiver queries file.

---
+++ src/queries/CareGiver.tsx
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag';
+
+const GET_CARE_GIVER_DETAILS = gql `
+
+`
+
+const ADD_CARE_GIVER = gql``;
+const UPDATE_CARE_GIVER = gql``;
+const GET_CARE_GIVER_DETAIL_BY_ID = gql ``;
+
+export const CareGiverQueries = {
+    GET_CARE_GIVER_DETAILS,
+    GET_CARE_GIVER_DETAIL_BY_ID,
+    ADD_CARE_GIVER,
+    UPDATE_CARE_GIVER
+}

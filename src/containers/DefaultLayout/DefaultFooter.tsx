ecb775085558c0ddf2b8e9d86e560ef4d9d1f5a1
rohit-chapter <rohit.chapter247@gmail.com>
2020-01-08 15:00:53 +0530

    design chnges sidebar in care giver page

--- src/containers/DefaultLayout/DefaultFooter.tsx
+++ src/containers/DefaultLayout/DefaultFooter.tsx
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import Select from "react-select";
+import { AppRoutes } from "../../config";
+import routes from "../../routes/routes";
+import { Region } from "../../config";
 
 class DefaultFooter extends Component {
   render() {
-    return <React.Fragment />;
+    return (
+      <React.Fragment>
+        <div className="d-flex align-items-center justify-content-between w-100">
+          <div className="footer-nav">
+            <Nav>
+              <NavItem>
+                <NavLink href="#">Attribute: Login Possible</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Unlocked</NavLink>
+              </NavItem>
+            </Nav>
+          </div>
+          <div className="region-select">
+            <div>
+              <div className=""></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
--- src/pages/CareGiver/Invoices/Invoices.tsx
+++ src/pages/CareGiver/Invoices/Invoices.tsx
@@ -4,22 +4,120 @@ class Invoices extends Component {
   render() {
     return (
       <>
-        <div className="carelogin-section">
+        <div className="careinvoice-section">
           <div>
             <h5 className="content-title">Invoices</h5>
             <Table bordered hover responsive>
               <thead className="thead-bg">
                 <tr>
+                  <th>Institution ID</th>
+                  <th>Cancellation For</th>
+                  <th>Cancelled By</th>
+                  <th>Facility</th>
                   <th>Date</th>
-                  <th>Computer</th>
-                  <th>Browser</th>
+                  <th>Amount</th>
+                  <th>Vat</th>
+                  <th>Posted Date</th>
+                  <th>Factoring</th>
+                  <th>Sent BF</th>
+                  <th>Remark</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="table-success">
-                  <td>20.08.2019 12:08:20</td>
-                  <td>94.138.88.227</td>
-                  <td>Mozila/5.0 (Windows NT 10.0; Win64; X64; rv68.0)</td>
+                <tr>
+                  <td colSpan={11}>
+                    <span>230001</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>230001</td>
+                  <td></td>
+                  <td>230002</td>
+                  <td>GZH</td>
+                  <td>29.04.2019</td>
+                  <td>2,190.50</td>
+                  <td>0%</td>
+                  <td>30.04.2019</td>
+                  <td>
+                    <span className="checkboxli checkbox-custom checkbox-default">
+                      <input type="checkbox" id="checkAll" className="" />
+                      <label className=""></label>
+                    </span>
+                  </td>
+                  <td>30.04.2019</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>230001</td>
+                  <td></td>
+                  <td>230002</td>
+                  <td>GZH</td>
+                  <td>29.04.2019</td>
+                  <td>2,190.50</td>
+                  <td>0%</td>
+                  <td>30.04.2019</td>
+                  <td>
+                    <span className="checkboxli checkbox-custom checkbox-default">
+                      <input type="checkbox" id="checkAll" className="" />
+                      <label className=""></label>
+                    </span>
+                  </td>
+                  <td>30.04.2019</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>230001</td>
+                  <td></td>
+                  <td>230002</td>
+                  <td>GZH</td>
+                  <td>29.04.2019</td>
+                  <td>2,190.50</td>
+                  <td>0%</td>
+                  <td>30.04.2019</td>
+                  <td>
+                    <span className="checkboxli checkbox-custom checkbox-default">
+                      <input type="checkbox" id="checkAll" className="" />
+                      <label className=""></label>
+                    </span>
+                  </td>
+                  <td>30.04.2019</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>230001</td>
+                  <td></td>
+                  <td>230002</td>
+                  <td>GZH</td>
+                  <td>29.04.2019</td>
+                  <td>2,190.50</td>
+                  <td>0%</td>
+                  <td>30.04.2019</td>
+                  <td>
+                    <span className="checkboxli checkbox-custom checkbox-default">
+                      <input type="checkbox" id="checkAll" className="" />
+                      <label className=""></label>
+                    </span>
+                  </td>
+                  <td>30.04.2019</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>230001</td>
+                  <td></td>
+                  <td>230002</td>
+                  <td>GZH</td>
+                  <td>29.04.2019</td>
+                  <td>2,190.50</td>
+                  <td>0%</td>
+                  <td>30.04.2019</td>
+                  <td>
+                    <span className="checkboxli checkbox-custom checkbox-default">
+                      <input type="checkbox" id="checkAll" className="" />
+                      <label className=""></label>
+                    </span>
+                  </td>
+                  <td>30.04.2019</td>
+                  <td></td>
                 </tr>
               </tbody>
             </Table>

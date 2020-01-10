8faccb0fb529038395bf8a1c8b0807a037eb2c93
Aayushi <ayushij.chapter247@gmail.com>
2020-01-10 11:10:25 +0530

    Call query

--- src/pages/Employee/AddEmployee/index.tsx
+++ src/pages/Employee/AddEmployee/index.tsx
@@ -3,9 +3,22 @@ import { Formik, FormikProps, FormikHelpers } from 'formik';
 import { EmployeeValidationSchema } from '../../../validations/EmployeeValidationSchema';
 import { IEmployeeFormValues, IEmployeeState } from '../../../interfaces';
 import EmployeeFormComponent from './EmployeeFormComponent';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-class EmployeeForm extends Component<any, IEmployeeState> {
-  handleSubmit = (
+const GET_USERS = gql`
+  query userList {
+    users {
+      id
+      firstName
+      lastName
+      email
+    }
+  }
+`;
+
+export const EmployeeForm = () => {
+  const handleSubmit = (
     values: IEmployeeFormValues,
     { setSubmitting }: FormikHelpers<IEmployeeFormValues>,
   ) => {
@@ -13,37 +26,36 @@ class EmployeeForm extends Component<any, IEmployeeState> {
     //to set submit state to false after successful signup
     setSubmitting(false);
   };
-
-  render() {
-    const values: IEmployeeFormValues = {
-      email: '',
-      firstName: '',
-      lastName: '',
-      userName: '',
-      telephoneNumber: '',
-      accountHolderName: '',
-      bankName: '',
-      IBAN: '',
-      BIC: '',
-      additionalText: '',
-      address1: '',
-      address2: '',
-      country: '',
-      zip: '',
-      joiningDate: '',
-      bankAccountNumber: '',
-    };
-    return (
-      <Formik
-        initialValues={values}
-        onSubmit={this.handleSubmit}
-        children={(props: FormikProps<IEmployeeFormValues>) => (
-          <EmployeeFormComponent {...props} />
-        )}
-        validationSchema={EmployeeValidationSchema}
-      />
-    );
-  }
-}
+  const { data, loading, error, refetch } = useQuery(GET_USERS);
+  console.log(data, 'dataaaaa');
+  const values: IEmployeeFormValues = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    userName: '',
+    telephoneNumber: '',
+    accountHolderName: '',
+    bankName: '',
+    IBAN: '',
+    BIC: '',
+    additionalText: '',
+    address1: '',
+    address2: '',
+    country: '',
+    zip: '',
+    joiningDate: '',
+    bankAccountNumber: '',
+  };
+  return (
+    <Formik
+      initialValues={values}
+      onSubmit={handleSubmit}
+      children={(props: FormikProps<IEmployeeFormValues>) => (
+        <EmployeeFormComponent {...props} />
+      )}
+      validationSchema={EmployeeValidationSchema}
+    />
+  );
+};
 
 export default EmployeeForm;

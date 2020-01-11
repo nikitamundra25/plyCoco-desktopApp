a8eb3bdd8a660d8a5fd3c508db4a63fdf1f22e9f
bhumikabalchandani8 <bhumika.chapter247@gmail.com>
2020-01-11 11:17:16 +0530

    make generic labels in json

--- src/validations/EmployeeValidationSchema.ts
+++ src/validations/EmployeeValidationSchema.ts
@@ -1,37 +1,37 @@
-import * as Yup from 'yup';
-import { IEmployeeFormValues, IDateResponse } from '../interfaces';
+import * as Yup from "yup";
+import { IEmployeeFormValues, IDateResponse } from "../interfaces";
 import {
   telephoneReqExp,
   nameRegExp,
   fileSize,
-  SupportedFormats,
-} from '../config';
-import { languageTranslation, logger, dateValidator } from '../helpers';
+  SupportedFormats
+} from "../config";
+import { languageTranslation, logger, dateValidator } from "../helpers";
 export const EmployeeValidationSchema: Yup.ObjectSchema<Yup.Shape<
   object,
   IEmployeeFormValues
 >> = Yup.object().shape<IEmployeeFormValues>({
   email: Yup.string()
     .trim()
-    .email(languageTranslation('VALID_EMAIL'))
-    .required(languageTranslation('REQUIRED_EMAIL')),
+    .email(languageTranslation("VALID_EMAIL"))
+    .required(languageTranslation("REQUIRED_EMAIL")),
   firstName: Yup.string()
     .trim()
-    .matches(nameRegExp, languageTranslation('FIRSTNAME_SPECIALCHARACTER'))
-    .max(20, languageTranslation('FIRSTNAME_MAXLENGTH'))
-    .required(languageTranslation('FIRSTNAME_REQUIRED')),
+    .matches(nameRegExp, languageTranslation("FIRSTNAME_SPECIALCHARACTER"))
+    .max(20, languageTranslation("FIRSTNAME_MAXLENGTH"))
+    .required(languageTranslation("FIRSTNAME_REQUIRED")),
   lastName: Yup.string()
     .trim()
-    .matches(nameRegExp, languageTranslation('LASTNAME_SPECIALCHARACTER'))
-    .max(20, languageTranslation('LASTNAME_MAXLENGTH'))
-    .required(languageTranslation('LASTNAME_REQUIRED')),
+    .matches(nameRegExp, languageTranslation("LASTNAME_SPECIALCHARACTER"))
+    .max(20, languageTranslation("LASTNAME_MAXLENGTH"))
+    .required(languageTranslation("LASTNAME_REQUIRED")),
   telephoneNumber: Yup.string().matches(
     telephoneReqExp,
-    languageTranslation('TELEPHONE_REQUIRED'),
+    languageTranslation("TELEPHONE_REQUIRED")
   ),
   userName: Yup.string()
     .trim()
-    .required(languageTranslation('USERNAME_REQUIRED')),
+    .required(languageTranslation("USERNAME_REQUIRED")),
   accountHolderName: Yup.string().trim(),
   bankName: Yup.string().trim(),
   IBAN: Yup.string(),
@@ -42,23 +42,23 @@ export const EmployeeValidationSchema: Yup.ObjectSchema<Yup.Shape<
   country: Yup.string(),
   zip: Yup.string(),
   joiningDate: Yup.mixed().test({
-    name: 'validate-date',
+    name: "validate-date",
     test: function(val) {
       const { path, createError } = this;
       const { isValid, message }: IDateResponse = dateValidator(val);
       return !val || isValid || createError({ path, message });
-    },
+    }
   }),
   bankAccountNumber: Yup.string(),
   image: Yup.mixed()
     .test(
-      'fileSize',
-      'File too large',
-      value => value && value.size <= fileSize,
+      "fileSize",
+      "File too large",
+      value => value && value.size <= fileSize
     )
     .test(
-      'fileFormat',
-      'Unsupported Format',
-      value => value && SupportedFormats.includes(value.type),
-    ),
+      "fileFormat",
+      "Unsupported Format",
+      value => value && SupportedFormats.includes(value.type)
+    )
 });

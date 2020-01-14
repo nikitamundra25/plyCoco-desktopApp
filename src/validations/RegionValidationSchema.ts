17ae19cb5bf43ac0e6195e1e52cf9d2090214030
bhumikabalchandani8 <bhumika.chapter247@gmail.com>
2020-01-14 14:27:10 +0530

    added required region label

--- src/validations/RegionValidationSchema.ts
+++ src/validations/RegionValidationSchema.ts
@@ -6,8 +6,5 @@ export const LoginValidationSchema: Yup.ObjectSchema<Yup.Shape<
   object,
   IRegionFormValue
 >> = Yup.object().shape<IRegionFormValue>({
-  nameOfRegion: Yup.string().required(languageTranslation("REQUIRED_EMAIL"))
-  //   password: Yup.string()
-  //     .min(6, languageTranslation("MIN_LENGTH_PASSWORD"))
-  //     .required(languageTranslation("REQUIRED_PASSWORD")),
+  nameOfRegion: Yup.string().required(languageTranslation("REQUIRED_REGION"))
 });

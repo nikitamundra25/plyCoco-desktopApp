5240831a5c8c13bedf521aefd2d65275ec0379b2
riyagupta247 <riya.chapter247@gmail.com>
2020-01-17 19:29:51 +0530

    # modified care giver validation schema.

--- src/validations/CareGiverValidationSchema.ts
+++ src/validations/CareGiverValidationSchema.ts
@@ -1,22 +1,21 @@
-import * as Yup from 'yup';
-import { CareGiverValues, IDateResponse } from '../interfaces';
-import { languageTranslation, dateValidator } from '../helpers';
-import { nameRegExp, telephoneReqExp } from '../config';
+import * as Yup from "yup";
+import { CareGiverValues, IDateResponse, IRemark } from "../interfaces";
+import { languageTranslation, dateValidator } from "../helpers";
+import { nameRegExp, telephoneReqExp } from "../config";
 
-export const CareGiverValidationSchema: Yup.ObjectSchema<Yup.Shape<
-  object,
-  CareGiverValues
->> = Yup.object().shape<CareGiverValues>({
+export const CareGiverValidationSchema: Yup.ObjectSchema<
+  Yup.Shape<object, CareGiverValues>
+> = Yup.object().shape<CareGiverValues>({
   firstName: Yup.string()
     .trim()
-    .matches(nameRegExp, languageTranslation('FIRSTNAME_SPECIALCHARACTER'))
-    .max(20, languageTranslation('FIRSTNAME_MAXLENGTH'))
-    .required(languageTranslation('FIRSTNAME_REQUIRED')),
+    .matches(nameRegExp, languageTranslation("FIRSTNAME_SPECIALCHARACTER"))
+    .max(20, languageTranslation("FIRSTNAME_MAXLENGTH"))
+    .required(languageTranslation("FIRSTNAME_REQUIRED")),
   lastName: Yup.string()
     .trim()
-    .matches(nameRegExp, languageTranslation('LASTNAME_SPECIALCHARACTER'))
-    .max(20, languageTranslation('LASTNAME_MAXLENGTH'))
-    .required(languageTranslation('LASTNAME_REQUIRED')),
+    .matches(nameRegExp, languageTranslation("LASTNAME_SPECIALCHARACTER"))
+    .max(20, languageTranslation("LASTNAME_MAXLENGTH"))
+    .required(languageTranslation("LASTNAME_REQUIRED")),
   address1: Yup.string(),
   address2: Yup.string(),
   street: Yup.string(),
@@ -31,12 +30,16 @@ export const CareGiverValidationSchema: Yup.ObjectSchema<Yup.Shape<
   //     return !val || isValid || createError({ path, message });
   //   },
   // }),
-  phoneNumber: Yup.string().min(10).max(13),
+  phoneNumber: Yup.string()
+    .min(10)
+    .max(13),
   fax: Yup.string(),
-  mobileNumber: Yup.string().min(10).max(13),
+  mobileNumber: Yup.string()
+    .min(10)
+    .max(13),
   userName: Yup.string()
     .trim()
-    .required(languageTranslation('USERNAME_REQUIRED')),
+    .required(languageTranslation("USERNAME_REQUIRED")),
   // bankName: Yup.string(),
   // .required(messages.REQUIRED_FIELD),
   companyName: Yup.string(),
@@ -45,9 +48,15 @@ export const CareGiverValidationSchema: Yup.ObjectSchema<Yup.Shape<
   executiveDirector: Yup.string(),
   socialSecurityContribution: Yup.boolean(),
   taxNumber: Yup.string(),
-  remarks: Yup.string(),
-  driversLicense:Yup.bool().default(false),
-    vehicleAvailable:Yup.bool().default(false),
-    driverLicenseNumber: Yup.string(),
-  status: Yup.string(),
+  remarks: Yup.array(
+    Yup.object().shape<IRemark>({
+      commentBy: Yup.string(),
+      description: Yup.string(),
+      commentAt: Yup.string()
+    })
+  ),
+  driversLicense: Yup.bool().default(false),
+  vehicleAvailable: Yup.bool().default(false),
+  driverLicenseNumber: Yup.string(),
+  status: Yup.string()
 });

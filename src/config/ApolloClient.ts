b23a0362c88da26d63fab8e977f6018b70f6c6ba
Sonu <sonu@chapter247.com>
2020-01-23 11:35:51 +0530

    Updated fetchPolicy to no cache

--- src/config/ApolloClient.ts
+++ src/config/ApolloClient.ts
@@ -3,7 +3,9 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
 import { AppConfig } from "./AppConfig";
 
-const cache: any = new InMemoryCache();
+const cache: any = new InMemoryCache({
+  dataIdFromObject: o => o.id
+});
 
 const link: any = createUploadLink({
   uri: AppConfig.GRAPHQL_ENDPOINT

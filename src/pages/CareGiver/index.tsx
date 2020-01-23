f1531200d220225f7a380bcc3228974729595054
rohit-chapter <rohit.chapter247@gmail.com>
2020-01-23 15:17:46 +0530

    design issues fix

--- src/pages/CareGiver/index.tsx
+++ src/pages/CareGiver/index.tsx
@@ -448,20 +448,20 @@ const CareGiver: FunctionComponent = () => {
                             </div>
                           </td>
                           <td>
-                            <div className="description-column  ml-0">
+                            <div className="description-column region-column ml-0">
                               {careGiverData &&
                               careGiverData.regions &&
                               careGiverData.regions.length ? (
-                                careGiverData.regions.map(
-                                  (wZ: any) => (
-                                    <p className="description-text ">
-                                      <span className="text-label mr-1">
-                                        <i className="fa fa-angle-right"></i>
-                                      </span>
-                                      <span className="align-middle">{wZ.regionName}</span>
-                                    </p>
-                                  )
-                                )
+                                careGiverData.regions.map((wZ: any) => (
+                                  <p className="description-text ">
+                                    <span className="text-label mr-1">
+                                      <i className="fa fa-angle-right"></i>
+                                    </span>
+                                    <span className="align-middle">
+                                      {wZ.regionName}
+                                    </span>
+                                  </p>
+                                ))
                               ) : (
                                 <div className="text-center">-</div>
                               )}

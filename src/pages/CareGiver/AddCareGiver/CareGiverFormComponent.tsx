4fa76fdd5e8454af4e2c34ef2a8dad3f709cc289
Aayushi <ayushij.chapter247@gmail.com>
2020-01-24 15:16:48 +0530

    Remarks on add caregiver

--- src/pages/CareGiver/AddCareGiver/CareGiverFormComponent.tsx
+++ src/pages/CareGiver/AddCareGiver/CareGiverFormComponent.tsx
@@ -1,8 +1,8 @@
-import React, { FunctionComponent, useEffect } from "react";
-import { Col, Row, Button } from "reactstrap";
-import "react-datepicker/dist/react-datepicker.css";
-import "react-datepicker/dist/react-datepicker.css";
-import { FormikProps, Form } from "formik";
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { Col, Row, Button } from 'reactstrap';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import { FormikProps, Form } from 'formik';
 import {
   CareGiverValues,
   IReactSelectInterface,
@@ -10,46 +10,51 @@ import {
   ICountries,
   IStates,
   IState,
-  ICareGiverValues
-} from "../../../interfaces";
+  ICareGiverValues,
+} from '../../../interfaces';
 import {
   CountryQueries,
   GET_QUALIFICATION_ATTRIBUTES,
-  GET_CAREGIVERS
-} from "../../../queries";
-import { useQuery, useLazyQuery } from "@apollo/react-hooks";
-import { languageTranslation } from "../../../helpers";
-import PersonalInfoFormComponent from "../PersonalInfo/PersonalInfoFormComponent";
-import BillingSettingsFormComponent from "../PersonalInfo/BillingSettingsFormComponent";
-import QualificationFormComponent from "../PersonalInfo/QualificationFormComponent";
-import AttributeFormComponent from "../PersonalInfo/AttributesFromComponent";
-import RemarkFormComponent from "../PersonalInfo/RemarkFormComponent";
-import "../caregiver.scss";
-import { IQualifications } from "../../../interfaces/qualification";
+  GET_CAREGIVERS,
+} from '../../../queries';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import { languageTranslation } from '../../../helpers';
+import PersonalInfoFormComponent from '../PersonalInfo/PersonalInfoFormComponent';
+import BillingSettingsFormComponent from '../PersonalInfo/BillingSettingsFormComponent';
+import QualificationFormComponent from '../PersonalInfo/QualificationFormComponent';
+import AttributeFormComponent from '../PersonalInfo/AttributesFromComponent';
+import RemarkFormComponent from '../PersonalInfo/RemarkFormComponent';
+import '../caregiver.scss';
+import { IQualifications } from '../../../interfaces/qualification';
 
 const [GET_COUNTRIES, GET_STATES_BY_COUNTRY] = CountryQueries;
 
 const CareGiverFormComponent: FunctionComponent<FormikProps<
   ICareGiverValues
->> = (props: FormikProps<ICareGiverValues>) => {
-  const { values } = props;
+> & { setRemarksDetail: any; remarksDetail: any }> = (
+  props: FormikProps<ICareGiverValues> & {
+    setRemarksDetail: any;
+    remarksDetail: any;
+  },
+) => {
+  const { values, setRemarksDetail, remarksDetail } = props;
   const handleField = (e: any) => {
     const value = {
       createdBy: `${values.firstName} ${values.lastName}`,
-      description: e.target.value
+      description: e.target.value,
     };
-    props.setFieldValue("remarks", [value]);
+    props.setFieldValue('remarks', [value]);
   };
   // To fetch the list of all caregiver
   const [fetchCareGivers, { data: careGivers }] = useLazyQuery<any>(
-    GET_CAREGIVERS
+    GET_CAREGIVERS,
   );
 
   // To fetch the list of countries
   const { data, loading, error, refetch } = useQuery<ICountries>(GET_COUNTRIES);
   // To fetch the states of selected contry & don't want to query on initial load
   const [getStatesByCountry, { data: statesData }] = useLazyQuery<IStates>(
-    GET_STATES_BY_COUNTRY
+    GET_STATES_BY_COUNTRY,
   );
   const countriesOpt: IReactSelectInterface[] | undefined = [];
   const statesOpt: IReactSelectInterface[] | undefined = [];
@@ -57,28 +62,28 @@ const CareGiverFormComponent: FunctionComponent<FormikProps<
     data.countries.forEach(({ id, name }: ICountry) =>
       countriesOpt.push({
         label: name,
-        value: id
-      })
+        value: id,
+      }),
     );
   }
   if (statesData && statesData.states) {
     statesData.states.forEach(({ id, name }: IState) =>
       statesOpt.push({
         label: name,
-        value: id
-      })
+        value: id,
+      }),
     );
   }
   // To fecth qualification attributes list
   const { data: qualificationData } = useQuery<IQualifications>(
-    GET_QUALIFICATION_ATTRIBUTES
+    GET_QUALIFICATION_ATTRIBUTES,
   );
   const qualificationList: IReactSelectInterface[] | undefined = [];
   if (qualificationData && qualificationData.getQualificationAttributes) {
     qualificationData.getQualificationAttributes.forEach((quali: any) => {
       qualificationList.push({
         label: quali.attributeName,
-        value: quali.id
+        value: quali.id,
       });
     });
   }
@@ -86,12 +91,12 @@ const CareGiverFormComponent: FunctionComponent<FormikProps<
     // Fetch list of caregivers
     fetchCareGivers({
       variables: {
-        searchBy: "",
+        searchBy: '',
         sortBy: 3,
         limit: 200,
         page: 1,
-        isActive: ""
-      }
+        isActive: '',
+      },
     });
   }, []);
   //Fetch careInstitutionList
@@ -104,41 +109,41 @@ const CareGiverFormComponent: FunctionComponent<FormikProps<
     careGivers.getCaregivers.result.forEach(
       ({ id, firstName, lastName }: any) =>
         careGiverOpt.push({
-          label: `${firstName}${" "}${lastName}`,
-          value: id
-        })
+          label: `${firstName}${' '}${lastName}`,
+          value: id,
+        }),
     );
   }
-  console.log("careGiverOpt", careGiverOpt);
+  console.log('careGiverOpt', careGiverOpt);
 
   return (
-    <Form className="form-section forms-main-section">
+    <Form className='form-section forms-main-section'>
       <Button
         disabled={props.isSubmitting}
-        id={"caregiver-add-btn"}
+        id={'caregiver-add-btn'}
         onClick={props.handleSubmit}
-        color={"primary"}
-        className={"save-button"}
+        color={'primary'}
+        className={'save-button'}
       >
         {props.isSubmitting ? (
-          <i className="fa fa-spinner fa-spin loader" />
+          <i className='fa fa-spinner fa-spin loader' />
         ) : (
-          ""
+          ''
         )}
 
-        {languageTranslation("SAVE_BUTTON")}
+        {languageTranslation('SAVE_BUTTON')}
       </Button>
-      <Row className={"m-0"}>
-        <Col lg={"4"}>
+      <Row className={'m-0'}>
+        <Col lg={'4'}>
           <PersonalInfoFormComponent
             {...props}
             CareInstitutionList={careGiverOpt}
           />
         </Col>
-        <Col lg={"4"} className="px-lg-0">
-          <div className="common-col">
+        <Col lg={'4'} className='px-lg-0'>
+          <div className='common-col'>
             <BillingSettingsFormComponent {...props} />
-            <div className="quality-attribute-section d-flex flex-column">
+            <div className='quality-attribute-section d-flex flex-column'>
               <QualificationFormComponent
                 {...props}
                 qualificationList={qualificationList}
@@ -147,7 +152,11 @@ const CareGiverFormComponent: FunctionComponent<FormikProps<
             </div>
           </div>
         </Col>
-        <RemarkFormComponent {...props} />
+        <RemarkFormComponent
+          {...props}
+          setRemarksDetail={setRemarksDetail}
+          remarksDetail={remarksDetail}
+        />
       </Row>
     </Form>
   );

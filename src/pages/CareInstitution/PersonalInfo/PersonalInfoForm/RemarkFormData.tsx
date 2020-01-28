29ffb006811040ae8b477b8bdf3540d8e103b888
rohit-chapter <rohit.chapter247@gmail.com>
2020-01-28 13:31:47 +0530

    custom scrollbar design

--- src/pages/CareInstitution/PersonalInfo/PersonalInfoForm/RemarkFormData.tsx
+++ src/pages/CareInstitution/PersonalInfo/PersonalInfoForm/RemarkFormData.tsx
@@ -75,198 +75,194 @@ const RemarkFormData: FunctionComponent<FormikProps<
   } = props;
 
   return (
-    <Col lg={4}>
-      <div className="remark-details ">
-        <div className="remark-header d-flex align-items-center justify-content-between">
-          <h5 className="my-2 text-left activity-title">
-            {" "}
-            {languageTranslation("REMARKS")}
-          </h5>
-        </div>
-        <div className="remark-body remark-body-max-height ">
-          <div className="activity-logs ">
+    <div className="remark-details ">
+      <div className="remark-header d-flex align-items-center justify-content-between">
+        <h5 className="my-2 text-left activity-title">
+          {" "}
+          {languageTranslation("REMARKS")}
+        </h5>
+      </div>
+      <div className="remark-body remark-body-max-height ">
+        <div className="activity-logs ">
+          <div>
             <div>
-              <div>
-                <div className="activity-block py-2 px-3">
-                  <div className=" text-left">
-                    <div className="remark-section">
-                      <Input
-                        type="textarea"
-                        name={"remarkData"}
-                        onChange={handleChange}
-                        placeholder="Enter your remark"
-                        value={remarkData}
-                        className="height-textarea"
-                        maxLength={1000}
-                      />
-                    </div>
+              <div className="activity-block py-2 px-3">
+                <div className=" text-left">
+                  <div className="remark-section">
+                    <Input
+                      type="textarea"
+                      name={"remarkData"}
+                      onChange={handleChange}
+                      placeholder="Enter your remark"
+                      value={remarkData}
+                      className="height-textarea"
+                      maxLength={1000}
+                    />
                   </div>
-                  <div className="activity-date position-relative">
-                    <span>
-                      <i className="fa fa-clock-o mr-2"></i>
-                      {moment().format("MMMM Do YYYY, h:mm a")}
-                    </span>
-                    <span>
-                      <i className="fa fa-user mr-2"></i>Mark Smith
-                    </span>
-                    <div className="remark-action-btn">
-                      <div
-                        className={`add-remark-btn ${
-                          !remarkData ? "disabled-div" : " "
-                        }`}
-                        onClick={e => {
-                          if (remarkData) {
-                            const temp = remarksDetail ? remarksDetail : [];
-                            temp.unshift({
-                              data: remarkData,
-                              createdAt: moment().format(
-                                "MMMM Do YYYY, h:mm a"
-                              ),
-                              createdBy: "john doe"
-                            });
-                            if (setRemarksDetail) {
-                              setRemarksDetail(temp);
-                              setFieldValue("remarkData", "");
-                            }
-                            if (props.saveRemark) {
-                              props.saveRemark(
-                                languageTranslation("REMARK_ADDED_SUCCESS"),
-                                undefined
+                </div>
+                <div className="activity-date position-relative">
+                  <span>
+                    <i className="fa fa-clock-o mr-2"></i>
+                    {moment().format("MMMM Do YYYY, h:mm a")}
+                  </span>
+                  <span>
+                    <i className="fa fa-user mr-2"></i>Mark Smith
+                  </span>
+                  <div className="remark-action-btn">
+                    <div
+                      className={`add-remark-btn ${
+                        !remarkData ? "disabled-div" : " "
+                      }`}
+                      onClick={e => {
+                        if (remarkData) {
+                          const temp = remarksDetail ? remarksDetail : [];
+                          temp.unshift({
+                            data: remarkData,
+                            createdAt: moment().format("MMMM Do YYYY, h:mm a"),
+                            createdBy: "john doe"
+                          });
+                          if (setRemarksDetail) {
+                            setRemarksDetail(temp);
+                            setFieldValue("remarkData", "");
+                          }
+                          if (props.saveRemark) {
+                            props.saveRemark(
+                              languageTranslation("REMARK_ADDED_SUCCESS"),
+                              undefined
+                            );
+                          } else {
+                            if (!toast.isActive(toastId)) {
+                              toast.success(
+                                languageTranslation("REMARK_ADDED_SUCCESS")
                               );
-                            } else {
-                              if (!toast.isActive(toastId)) {
-                                toast.success(
-                                  languageTranslation("REMARK_ADDED_SUCCESS")
-                                );
-                              }
                             }
                           }
-                        }}
-                      >
-                        <i className={"fa fa-plus"} />
-                        &nbsp; Add More
-                      </div>
+                        }
+                      }}
+                    >
+                      <i className={"fa fa-plus"} />
+                      &nbsp; Add More
                     </div>
                   </div>
-                  <span className="activity-icon activity-set"></span>
                 </div>
+                <span className="activity-icon activity-set"></span>
               </div>
             </div>
+          </div>
 
-            {remarksDetail && remarksDetail.length ? (
-              <>
-                {remarksDetail.map(
-                  (remark: ICareInstitutionRemarks, index: number) => {
-                    return (
-                      <div className="activity-block py-2" key={index}>
-                        <div>
-                          <div className="remark-section">
-                            {activeRemark === index && isEditRemark ? (
-                              <Input
-                                type="textarea"
-                                name={"remarkValue"}
-                                onChange={handleChange}
-                                placeholder="Remarks"
-                                value={remarkValue}
-                                maxLength={1000}
-                                className="height-textarea "
-                              />
-                            ) : (
-                              remark.data
-                            )}
-                          </div>
+          {remarksDetail && remarksDetail.length ? (
+            <>
+              {remarksDetail.map(
+                (remark: ICareInstitutionRemarks, index: number) => {
+                  return (
+                    <div className="activity-block py-2" key={index}>
+                      <div>
+                        <div className="remark-section">
+                          {activeRemark === index && isEditRemark ? (
+                            <Input
+                              type="textarea"
+                              name={"remarkValue"}
+                              onChange={handleChange}
+                              placeholder="Remarks"
+                              value={remarkValue}
+                              maxLength={1000}
+                              className="height-textarea "
+                            />
+                          ) : (
+                            remark.data
+                          )}
                         </div>
-                        <div className=" activity-date position-relative">
-                          <span>
-                            <i className="fa fa-clock-o mr-2"></i>
-                            {remark.createdAt}
-                          </span>
-                          <span>
-                            <i className="fa fa-user mr-2"></i>Mark Smith
-                          </span>
-                          <div className="remark-action-btn">
-                            <span
-                              id={`edit${index}`}
-                              onClick={() => {
-                                // To update the remark
-                                if (isEditRemark) {
-                                  if (remarkValue) {
-                                    const temp = remarksDetail
-                                      ? remarksDetail
-                                      : [];
-                                    temp[activeRemark].data = remarkValue;
-                                    if (setRemarksDetail) {
-                                      setRemarksDetail(temp);
-                                      setFieldValue("remarkValue", "");
-                                      setisEditRemark(false);
-                                    }
-                                    if (props.saveRemark) {
-                                      props.saveRemark(
-                                        languageTranslation(
-                                          "REMARK_UPDATE_SUCCESS"
-                                        ),
-                                        undefined
-                                      );
-                                    } else {
-                                      toast.success(
-                                        languageTranslation(
-                                          "REMARK_UPDATE_SUCCESS"
-                                        )
-                                      );
-                                    }
+                      </div>
+                      <div className=" activity-date position-relative">
+                        <span>
+                          <i className="fa fa-clock-o mr-2"></i>
+                          {remark.createdAt}
+                        </span>
+                        <span>
+                          <i className="fa fa-user mr-2"></i>Mark Smith
+                        </span>
+                        <div className="remark-action-btn">
+                          <span
+                            id={`edit${index}`}
+                            onClick={() => {
+                              // To update the remark
+                              if (isEditRemark) {
+                                if (remarkValue) {
+                                  const temp = remarksDetail
+                                    ? remarksDetail
+                                    : [];
+                                  temp[activeRemark].data = remarkValue;
+                                  if (setRemarksDetail) {
+                                    setRemarksDetail(temp);
+                                    setFieldValue("remarkValue", "");
+                                    setisEditRemark(false);
+                                  }
+                                  if (props.saveRemark) {
+                                    props.saveRemark(
+                                      languageTranslation(
+                                        "REMARK_UPDATE_SUCCESS"
+                                      ),
+                                      undefined
+                                    );
+                                  } else {
+                                    toast.success(
+                                      languageTranslation(
+                                        "REMARK_UPDATE_SUCCESS"
+                                      )
+                                    );
                                   }
                                 }
-                                // To convert into text field on edit
-                                else {
-                                  setisEditRemark(true);
-                                  setActiveRemark(index);
-                                  setFieldValue("remarkValue", remark.data);
-                                }
-                              }}
-                              className="edit-btn cursor-pointer"
+                              }
+                              // To convert into text field on edit
+                              else {
+                                setisEditRemark(true);
+                                setActiveRemark(index);
+                                setFieldValue("remarkValue", remark.data);
+                              }
+                            }}
+                            className="edit-btn cursor-pointer"
+                          >
+                            {activeRemark === index && isEditRemark ? (
+                              <i className="fa fa-check"></i>
+                            ) : (
+                              <i className="icon-note"></i>
+                            )}
+                            <UncontrolledTooltip
+                              placement="top"
+                              target={`edit${index}`}
                             >
-                              {activeRemark === index && isEditRemark ? (
-                                <i className="fa fa-check"></i>
-                              ) : (
-                                <i className="icon-note"></i>
-                              )}
-                              <UncontrolledTooltip
-                                placement="top"
-                                target={`edit${index}`}
-                              >
-                                {activeRemark === index && isEditRemark
-                                  ? languageTranslation("REMARK_UPDATE")
-                                  : languageTranslation("REMARK_EDIT")}
-                              </UncontrolledTooltip>
-                            </span>
-                            <span
-                              onClick={() => onDelete(index)}
-                              className="delete-btn cursor-pointer"
-                              id={`delete${index}`}
+                              {activeRemark === index && isEditRemark
+                                ? languageTranslation("REMARK_UPDATE")
+                                : languageTranslation("REMARK_EDIT")}
+                            </UncontrolledTooltip>
+                          </span>
+                          <span
+                            onClick={() => onDelete(index)}
+                            className="delete-btn cursor-pointer"
+                            id={`delete${index}`}
+                          >
+                            <UncontrolledTooltip
+                              placement="top"
+                              target={`delete${index}`}
                             >
-                              <UncontrolledTooltip
-                                placement="top"
-                                target={`delete${index}`}
-                              >
-                                {languageTranslation("REMARK_DELETE")}
-                              </UncontrolledTooltip>
-                              <i className="icon-trash"></i>
-                            </span>
-                          </div>
+                              {languageTranslation("REMARK_DELETE")}
+                            </UncontrolledTooltip>
+                            <i className="icon-trash"></i>
+                          </span>
                         </div>
-                        <span className="activity-icon activity-set"></span>
                       </div>
-                    );
-                  }
-                )}
-              </>
-            ) : (
-              ""
-            )}
-          </div>
+                      <span className="activity-icon activity-set"></span>
+                    </div>
+                  );
+                }
+              )}
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </div>
-    </Col>
+    </div>
   );
 };
 

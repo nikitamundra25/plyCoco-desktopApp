b11aa0c2d9581c98c3a9211e2068717d53905337
rohit-chapter <rohit.chapter247@gmail.com>
2020-02-06 15:29:01 +0530

    design issues fix

--- src/resource/views/pages/CareGiver/Emails/EmailFormComponent.tsx
+++ src/resource/views/pages/CareGiver/Emails/EmailFormComponent.tsx
@@ -1,63 +1,63 @@
-import React, { FunctionComponent } from 'react';
-import { Col, Row, FormGroup } from 'reactstrap';
-import { Editor } from 'react-draft-wysiwyg';
-import { convertToRaw } from 'draft-js';
-import draftToHtml from 'draftjs-to-html';
-import { IEmailFormComponentPorps } from '../../../../../interfaces';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { stripHtml, languageTranslation } from '../../../../../helpers';
+import React, { FunctionComponent } from "react";
+import { Col, Row, FormGroup } from "reactstrap";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import { IEmailFormComponentPorps } from "../../../../../interfaces";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { stripHtml, languageTranslation } from "../../../../../helpers";
 
 export const EmailFormComponent: FunctionComponent<IEmailFormComponentPorps> = (
-  props: IEmailFormComponentPorps,
+  props: IEmailFormComponentPorps
 ) => {
   const { body, onEditorStateChange, isSubmit } = props;
-  let content = body ? draftToHtml(convertToRaw(body.getCurrentContent())) : '';
+  let content = body ? draftToHtml(convertToRaw(body.getCurrentContent())) : "";
   const result = stripHtml(content);
   return (
-    <div className='form-card'>
+    <div>
       <Row>
-        <Col lg={'12'}>
+        <Col lg={"12"}>
           <FormGroup>
             <Row>
-              <Col sm='12'>
+              <Col sm="12">
                 <div>
                   <Editor
                     editorState={body}
-                    toolbarClassName='toolbarClassName'
-                    wrapperClassName='wrapperClassName'
-                    editorClassName='editorClassName'
-                    placeholder='Enter Email Here'
+                    toolbarClassName="toolbarClassName"
+                    wrapperClassName="wrapperClassName"
+                    editorClassName="editorClassName"
+                    placeholder="Enter Email Here"
                     toolbar={{
                       options: [
-                        'inline',
-                        'blockType',
-                        'fontSize',
-                        'list',
-                        'textAlign',
-                        'link',
+                        "inline",
+                        "blockType",
+                        "fontSize",
+                        "list",
+                        "textAlign",
+                        "link"
                       ],
                       inline: {
-                        options: ['bold', 'italic', 'underline'],
+                        options: ["bold", "italic", "underline"]
                       },
                       fontSize: {
-                        className: 'bordered-option-classname',
+                        className: "bordered-option-classname"
                       },
                       fontFamily: {
-                        className: 'bordered-option-classname',
+                        className: "bordered-option-classname"
                       },
                       list: {
                         inDropdown: false,
-                        options: ['unordered'],
+                        options: ["unordered"]
                       },
                       link: {
-                        options: ['link'],
-                      },
+                        options: ["link"]
+                      }
                     }}
                     onEditorStateChange={onEditorStateChange}
                   />
                   {isSubmit && (!body || (result && result.length < 2)) ? (
-                    <div className='required-error'>
-                      {languageTranslation('REQUIRED_BODY')}
+                    <div className="required-error">
+                      {languageTranslation("REQUIRED_BODY")}
                     </div>
                   ) : null}
                 </div>

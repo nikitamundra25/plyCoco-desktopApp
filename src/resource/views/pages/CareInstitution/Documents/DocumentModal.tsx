129318141fd11490a231a8e248106b3aa2f11fbd
Rishabh Bula <rishab.chapter247@gmail.com>
2020-02-07 10:21:38 +0530

    added  document templete in care institution

---
+++ src/resource/views/pages/CareInstitution/Documents/DocumentModal.tsx
@@ -0,0 +1,239 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalTitle,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Row,
+  Form
+} from "reactstrap";
+import Select from "react-select";
+import "react-day-picker/lib/style.css";
+import { languageTranslation } from "../../../../../helpers";
+import { DocumentTypes } from "../../../../../config";
+import { useDropzone } from "react-dropzone";
+
+import png from "../../../../assets/img/png.svg";
+import jpg from "../../../../assets/img/jpg.svg";
+import pdf from "../../../../assets/img/pdf.svg";
+import close from "../../../../assets/img/cancel.svg";
+import closehover from "../../../../assets/img/cancel-hover.svg";
+const DocumentUploadModal = (props: any) => {
+  // const [statusValue, setStatusValue] = useState<boolean>(true);
+  // const [remarkValue, setRemarkValue] = useState<any>(null);
+  // const [documentType, setDocumentType] = useState<any>(null);
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.name === 'check') {
+  //     setStatusValue(e.target.checked);
+  //   } /*  (e.target.name === 'remarks') */ else {
+  //     setRemarkValue(e.target.value);
+  //   }
+  // };
+  const { show, handleClose } = props;
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: props.onDrop,
+    multiple: false
+  });
+  const externalCloseBtn = (
+    <button className="close modal-close" onClick={() => handleClose()}>
+      <img src={close} alt="close" className="main-img" />
+      <img src={closehover} alt="close" className="hover-img" />
+    </button>
+  );
+  return (
+    <div>
+      <Modal isOpen={show} className="reminder-modal" size="lg" centered>
+        <ModalHeader close={externalCloseBtn}>
+          {languageTranslation("ADD_DOCUMENT")}
+        </ModalHeader>
+        <ModalBody>
+          <div className="">
+            <Form className="form-section forms-main-section">
+              <Row>
+                <Col lg={"12"}>
+                  <FormGroup>
+                    <Row>
+                      <Col sm="2">
+                        <Label className="form-label col-form-label">
+                          {languageTranslation("FILE")}
+                        </Label>
+                      </Col>
+                      <Col sm="10">
+                        <div
+                          {...getRootProps()}
+                          className="dropzone-preview mb-0"
+                        >
+                          <input
+                            {...getInputProps()}
+                            className="dropzone-input-preview"
+                          />
+
+                          <div className="icon-upload">
+                            <i className="cui-cloud-upload"></i>
+                            {/* <img src={png} alt='' className='mb-2' />
+                            <img src={jpg} alt='' className='mb-2' />
+                            <img src={pdf} alt='' className='mb-2' /> */}
+                          </div>
+                          <span>
+                            {props && props.documentUrls
+                              ? props.documentUrls.name
+                              : "Drag 'n' drop files here, or click here to upload files"}
+                          </span>
+                        </div>
+                        {/* <div>
+                          <div {...getRootProps()}>
+                            <input
+                              {...getInputProps()}
+                              className='dropzone-input-preview'
+                            />
+                            <span>
+                              Drag 'n' drop some files here, or click to select
+                              files
+                            </span>
+                          </div>
+                          <span>
+                            {documentUrls
+                              ? documentUrls.name
+                              : "Drag 'n' drop files here, or click here to upload files"}
+                          </span>
+                        </div>
+                        {props && props.documentUrls
+                          ? props.documentUrls.name
+                          : null} */}
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Col>
+                <Col lg={"12"}>
+                  <FormGroup>
+                    <Row>
+                      <Col sm="2">
+                        <Label className="form-label col-form-label">
+                          {languageTranslation("TYPE")}
+                        </Label>
+                      </Col>
+                      <Col sm="10">
+                        <Select
+                          name="type"
+                          options={DocumentTypes}
+                          placeholder={"Select type"}
+                          onChange={(type: any) => {
+                            props.setDocumentType(type);
+                          }}
+                          classNamePrefix="custom-inner-reactselect"
+                          className={"custom-reactselect"}
+                        />
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Col>
+                <Col lg={"12"}>
+                  <FormGroup>
+                    <Row>
+                      <Col sm="2">
+                        <Label className="form-label col-form-label">
+                          {languageTranslation("REMARKS")}
+                        </Label>
+                      </Col>
+                      <Col sm="10">
+                        <div>
+                          <Input
+                            type="textarea"
+                            placeholder={languageTranslation("REMARKS")}
+                            className="textarea-custom"
+                            rows="4"
+                            name={"remarks"}
+                            value={props.remarkValue}
+                            onChange={props.handleChange}
+                          />
+                        </div>
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Col>
+                <Col lg={"12"}>
+                  <FormGroup>
+                    <Row>
+                      <Col sm="2">
+                        <Label className="form-label col-form-label">
+                          {languageTranslation("CHECKED")}
+                        </Label>
+                      </Col>
+                      <Col sm="10">
+                        <div className=" checkbox-custom mb-0">
+                          <input
+                            id="check"
+                            type="checkbox"
+                            name="check"
+                            checked={props.statusValue}
+                            onChange={props.handleChange}
+                          />
+                          <Label for="check" className="pl-3">
+                            ( {languageTranslation("DOCUMENT_STATUS_LABEL")} )
+                          </Label>
+                        </div>
+                        {/* <div>
+                          <Input
+                            type='checkbox'
+                            name='check'
+                            checked={props.statusValue}
+                            onChange={props.handleChange}
+                          />
+                          <span>
+                            (checked files cannot be punched by the user)
+                          </span>
+                        </div> */}
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Col>
+                {/* <Col lg={'12'}>
+                  <FormGroup>
+                    <Row>
+                      <Col sm='2'>
+                        <Label className='form-label col-form-label'>
+                          {languageTranslation("OPTIMIZE")}
+                        </Label>
+                      </Col>
+                      <Col sm='10'>
+                       
+                          <div className=" checkbox-custom mb-0">
+                          <input
+                            type="checkbox"
+                            id="check"
+                            className=""
+                            name={"employed"}
+                          />
+                          <Label for="check" className="pl-3">
+                            ( {languageTranslation("DOCUMENT_OPTIMIZE_LABEL")} )
+                          </Label>
+                        </div>
+                       
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Col> */}
+              </Row>
+            </Form>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={props.handleSaveDocument}>
+            {languageTranslation("SAVE_BUTTON")}
+          </Button>
+          <Button color="secondary" onClick={handleClose}>
+            {languageTranslation("CANCEL")}
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default DocumentUploadModal;
--- src/resource/views/pages/CareInstitution/Documents/index.tsx
+++ src/resource/views/pages/CareInstitution/Documents/index.tsx
@@ -1,158 +1,215 @@
-import React, { Component, FunctionComponent } from 'react';
-import { Card, Table, Row, Col, FormGroup } from 'reactstrap';
-import { RouteComponentProps } from 'react-router';
-import { languageTranslation } from '../../../../../helpers';
-import Select from 'react-select';
-import { State } from '../../../../../config';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Card, CardHeader, CardBody } from 'reactstrap';
+import { useMutation, useLazyQuery } from '@apollo/react-hooks';
+import { AppBreadcrumb } from '@coreui/react';
 
-const Documents: FunctionComponent = () => {
+import { toast } from 'react-toastify';
+import { useLocation } from 'react-router-dom';
+import { DocumentMutations } from '../../../../../graphql/Mutations';
+import routes from '../../../../../routes/routes';
+import moment from 'moment';
+import { IDocumentUrls } from '../../../../../interfaces';
+import DocumentUploadModal from './DocumentModal';
+import DocumentsList from './DocumentsList';
+import { DocumentQueries } from '../../../../../graphql/queries';
+import { languageTranslation } from '../../../../../helpers';
+import { ConfirmBox } from '../../../components/ConfirmBox';
+const [
+  ADD_DOCUMENT,
+  UPDATE_DOCUMENT_STATUS,
+  UPDATE_DOCUMENT
+] = DocumentMutations;
+const [GET_DOCUMENT_LIST] = DocumentQueries;
+let toastId: any = '';
+
+const Documents = () => {
+  const path = useLocation();
+  const queryPath = path.pathname;
+  const res = queryPath.split('/');
+  const id = parseInt(res[3]);
+  const [showDocumentPopup, setShowDocumentPopup] = useState<boolean>(false);
+  const [documentUrls, setDocumentUrl] = useState<IDocumentUrls | null>(null);
+  const [fileObject, setFileObject] = useState<Object | null>(null);
+  const [statusValue, setStatusValue] = useState<boolean>(true);
+  const [remarkValue, setRemarkValue] = useState<any>(null);
+  const [documentType, setDocumentType] = useState<any>(null);
+  const [fetchDocumentList, { data, loading, refetch }] = useLazyQuery<any>(
+    GET_DOCUMENT_LIST
+  );
+  const [documentId, setDocumentId] = useState<{
+    id: string;
+    checked: boolean;
+  } | null>(null);
+
+  //add document
+  const [addDocument] = useMutation<any>(ADD_DOCUMENT, {
+    onCompleted({ addDocument }) {
+      refetch();
+      setShowDocumentPopup(false);
+      if (!toast.isActive(toastId)) {
+        toastId = toast.success(languageTranslation('DOCUMENT_ADDED_SUCCESS'));
+      }
+    }
+  });
+
+  //update document status
+
+  const [updateDocumentStatus] = useMutation<any>(UPDATE_DOCUMENT_STATUS);
+  //   , {
+  //   onCompleted({ updateDocumentStatus }) {
+  //     refetch();
+  //     if (!toast.isActive(toastId)) {
+  //       toastId = toast.success(
+  //         languageTranslation('DOCUMENT_STATUS_UPDATED_SUCCESS')
+  //       );
+  //     }
+  //   }
+  // });
+
+  //update document
+  const [updateDocument] = useMutation<any>(UPDATE_DOCUMENT, {
+    onCompleted({ updateDocument }) {
+      refetch();
+      setShowDocumentPopup(false);
+      if (!toast.isActive(toastId)) {
+        toastId = toast.success(
+          languageTranslation('DOCUMENT_UPDATED_SUCCESS')
+        );
+      }
+    }
+  });
+
+  useEffect(() => {
+    if (id) {
+      fetchDocumentList({
+        variables: {
+          userId: id ? id : ''
+        }
+      });
+    }
+  }, []);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    const { checked, name, value } = target;
+    if (name === 'check') {
+      setStatusValue(checked);
+    } else {
+      setRemarkValue(value);
+    }
+  };
+
+  //convert document to binary format
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    let temp: any = documentUrls ? documentUrls : {};
+    acceptedFiles.forEach((file: File) => {
+      setFileObject(file);
+      if (file) {
+        const reader = new FileReader();
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+        reader.onloadend = () => {
+          if (reader.result) {
+            temp = {
+              path: reader.result,
+              name: file.name,
+              date: moment().format('DD.MM.YYYY')
+            };
+            setDocumentUrl(temp);
+          }
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  }, []);
+
+  //approve disapprove checkbox
+  const handleCheckElement = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: string,
+    status: string
+  ) => {
+    const { target } = e;
+    const { checked } = target;
+    setDocumentId({ id, checked });
+    const { value } = await ConfirmBox({
+      title: languageTranslation('CONFIRM_LABEL'),
+      text: languageTranslation(
+        status === 'notrequested'
+          ? 'CONFIRM_CAREGIVER_DOCUMENT_STATUS_APPROVE_MSG'
+          : 'CONFIRM_CAREGIVER_DOCUMENT_STATUS_NOTREQUESTED_MSG'
+      )
+    });
+    if (!value) {
+      setDocumentId(null);
+      return;
+    } else {
+      try {
+        // toast.dismiss();
+        console.log('id in upd', id);
+        await updateDocumentStatus({
+          variables: {
+            id: id ? parseInt(id) : null,
+            status: checked === true ? 'approve' : 'decline'
+          }
+        });
+        setDocumentId(null);
+        refetch();
+        if (!toast.isActive(toastId)) {
+          toastId = toast.success(
+            languageTranslation('DOCUMENT_STATUS_UPDATED_SUCCESS')
+          );
+        }
+      } catch (error) {
+        const message = error.message
+          .replace('SequelizeValidationError: ', '')
+          .replace('Validation error: ', '')
+          .replace('GraphQL error: ', '');
+        if (!toast.isActive(toastId)) {
+          toastId = toast.error(message);
+        }
+      }
+    }
+  };
+  //on save document detatils
+  const handleSaveDocument = () => {
+    const queryPath = path.pathname;
+    const res = queryPath.split('/');
+    const id = parseInt(res[3]);
+    if (id) {
+      addDocument({
+        variables: {
+          documentInput: {
+            userId: id ? id : '',
+            document: fileObject ? fileObject : null,
+            remarks: remarkValue,
+            status: statusValue ? 'approve' : 'notrequested',
+            documentType: documentType ? documentType.value : ''
+          }
+        }
+      });
+    }
+  };
+  console.log('fileObject in index', fileObject);
+
   return (
-    <div className='document-upload-section mb-3'>
-      <Table bordered hover responsive>
-        <thead className='thead-bg'>
-          <tr>
-            <th>{languageTranslation('S_NO')}</th>
-            <th>{languageTranslation('DATE')}</th>
-            <th>{languageTranslation('FILE_NAME')}</th>
-            <th>{languageTranslation('TYPE')}</th>
-            <th>{languageTranslation('REMARKS')}</th>
-            <th>{languageTranslation('STATUS')}</th>
-            <th>{languageTranslation('FILE_SIZE')}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>20.08.2019 12:08:20</td>
-            <td>License.pdf</td>
-            <td>Diploama/Exam</td>
-            <td></td>
-            <td>
-              <span className='checkboxli checkbox-custom checkbox-default'>
-                <input type='checkbox' id='checkAll' className='' />
-                <label className=''></label>
-              </span>
-            </td>
-            <td>162KB</td>
-          </tr>
-        </tbody>
-      </Table>
-      <div>
-        <Row className='common-offer-row'>
-          <Col md={4}>
-            <div className='common-list-wrap'>
-              <div className='common-list-header d-flex align-items-cente justify-content-between'>
-                <div className='common-list-title align-middle'>
-                  {languageTranslation('TYPE')}{' '}
-                </div>
-                <div className=' align-middle toggle-icon'>
-                  <i className='fa fa-angle-down'></i>
-                </div>
-              </div>
-              <div className='common-list-body'>
-                <ul className='common-list list-unstyled'>
-                  <li>File Type 1 </li>
-                  <li>File Type 2</li>
-                  <li>File Type 3</li>
-                </ul>
-              </div>
-              <div className='common-list-footer form-section '>
-                <FormGroup className='mb-0'>
-                  <Select
-                    placeholder={languageTranslation('TYPE')}
-                    options={State}
-                    menuPlacement={'top'}
-                  />
-                </FormGroup>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </div>
+    <div>
+      <DocumentsList
+        setShowDocumentPopup={setShowDocumentPopup}
+        documentListing={data}
+        handleCheckElement={handleCheckElement}
+        documentId={documentId}
+      />
+      <DocumentUploadModal
+        show={showDocumentPopup}
+        handleClose={() => setShowDocumentPopup(false)}
+        handleSaveDocument={handleSaveDocument}
+        onDrop={onDrop}
+        documentUrls={documentUrls}
+        handleChange={handleChange}
+        setDocumentType={setDocumentType}
+        remarkValue={remarkValue}
+        statusValue={statusValue}
+      />
     </div>
   );
 };

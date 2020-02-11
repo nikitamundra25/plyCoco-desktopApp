d474962fe548e4d8ec7b41a0fe385cc28035d5f1
Rishabh Bula <rishab.chapter247@gmail.com>
2020-02-11 10:38:20 +0530

    fix multi checkbox issue

--- src/resource/views/pages/BulkEmailCaregiver/index.tsx
+++ src/resource/views/pages/BulkEmailCaregiver/index.tsx
@@ -1,19 +1,19 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
-import { FormGroup, Label, Input, Col, Row, Table } from "reactstrap";
-import Select from "react-select";
-import { languageTranslation } from "../../../../helpers";
-import { CareGiverQueries } from "../../../../graphql/queries";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { State } from "../../../../config";
-import filter from "../../../assets/img/filter.svg";
-import refresh from "../../../assets/img/refresh.svg";
-import send from "../../../assets/img/send.svg";
-import "./index.scss";
-import { useLazyQuery } from "@apollo/react-hooks";
-import Loader from "../../containers/Loader/Loader";
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { FormGroup, Label, Input, Col, Row, Table } from 'reactstrap';
+import Select from 'react-select';
+import { languageTranslation } from '../../../../helpers';
+import { CareGiverQueries } from '../../../../graphql/queries';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { State } from '../../../../config';
+import filter from '../../../assets/img/filter.svg';
+import refresh from '../../../assets/img/refresh.svg';
+import send from '../../../assets/img/send.svg';
+import './index.scss';
+import { useLazyQuery } from '@apollo/react-hooks';
+import Loader from '../../containers/Loader/Loader';
 
-const [GET_CAREGIVERS] = CareGiverQueries;
+const [, , , , , , GET_CAREGIVERS_FOR_BULK_EMAIL] = CareGiverQueries;
 
 const BulkEmailCaregiver: FunctionComponent = () => {
   let [selectedCareGiver, setselectedCareGiver] = useState<any>([]);
@@ -21,22 +21,30 @@ const BulkEmailCaregiver: FunctionComponent = () => {
   const [
     fetchCareGiverList,
     { data: careGivers, called, loading, refetch }
-  ] = useLazyQuery<any, any>(GET_CAREGIVERS, {
-    fetchPolicy: "no-cache"
+  ] = useLazyQuery<any, any>(GET_CAREGIVERS_FOR_BULK_EMAIL, {
+    fetchPolicy: 'no-cache'
   });
 
   useEffect(() => {
     // Fetch list of caregivers
     fetchCareGiverList({
       variables: {
-        searchBy: "",
+        searchBy: '',
         sortBy: 3,
         limit: 200,
         page: 1,
-        isActive: ""
+        isActive: ''
       }
     });
   }, []);
+  const [careGiverData, setcareGiverData] = useState<Object[]>([]);
+  useEffect(() => {
+    if (careGivers) {
+      const { getCaregivers } = careGivers;
+      const { result } = getCaregivers;
+      setcareGiverData(result);
+    }
+  }, [careGivers]);
 
   const handleSelectAll = async () => {
     if (
@@ -70,65 +78,66 @@ const BulkEmailCaregiver: FunctionComponent = () => {
         id
       ]);
       return;
-    }
-    if (selectedCareGiver.indexOf(id) > -1) {
-      selectedCareGiver.splice(selectedCareGiver.indexOf(id), 1);
-
-      setselectedCareGiver(selectedCareGiver);
+    }else{
+      console.log("Not Selected");
+      if (selectedCareGiver.indexOf(id) > -1) {
+        selectedCareGiver.splice(selectedCareGiver.indexOf(id), 1);
+        setselectedCareGiver([...selectedCareGiver]);
+      }
     }
   };
 
-  console.log("selectedCareGiver ", selectedCareGiver);
+  console.log('selectedCareGiver ', selectedCareGiver);
 
   return (
     <>
-      <div className="common-detail-page">
-        <div className="common-detail-section">
-          <div className="sticky-common-header">
-            <div className="common-topheader d-flex align-items-center px-2 mb-1">
-              <div className="header-nav-item">
-                <span className="header-nav-icon">
-                  <img src={refresh} alt="" />
+      <div className='common-detail-page'>
+        <div className='common-detail-section'>
+          <div className='sticky-common-header'>
+            <div className='common-topheader d-flex align-items-center px-2 mb-1'>
+              <div className='header-nav-item'>
+                <span className='header-nav-icon'>
+                  <img src={refresh} alt='' />
                 </span>
-                <span className="header-nav-text">
-                  {languageTranslation("REFRESH")}
+                <span className='header-nav-text'>
+                  {languageTranslation('REFRESH')}
                 </span>
               </div>
-              <div className="header-nav-item">
-                <span className="header-nav-icon">
-                  <img src={filter} alt="" />
+              <div className='header-nav-item'>
+                <span className='header-nav-icon'>
+                  <img src={filter} alt='' />
                 </span>
-                <span className="header-nav-text">
-                  {languageTranslation("ATTRIBUTES")}
+                <span className='header-nav-text'>
+                  {languageTranslation('ATTRIBUTES')}
                 </span>
               </div>
 
-              <div className="header-nav-item">
-                <span className="header-nav-icon">
-                  <img src={send} alt="" />
+              <div className='header-nav-item'>
+                <span className='header-nav-icon'>
+                  <img src={send} alt='' />
                 </span>
-                <span className="header-nav-text">
-                  {languageTranslation("SEND")}
+                <span className='header-nav-text'>
+                  {languageTranslation('SEND')}
                 </span>
               </div>
             </div>
           </div>
 
-          <div className="common-content flex-grow-1">
-            <div className="bulk-email-section">
+          <div className='common-content flex-grow-1'>
+            <div className='bulk-email-section'>
               <Row>
-                <Col lg={"5"}>
-                  <div className="caregiver-list custom-scroll">
+                <Col lg={'5'}>
+                  <div className='caregiver-list custom-scroll'>
                     <Table bordered hover responsive>
-                      <thead className="thead-bg">
+                      <thead className='thead-bg'>
                         <tr>
-                          <th className="checkbox-th-column">
-                            <span className="checkboxli checkbox-custom checkbox-default mr-2">
+                          <th className='checkbox-th-column'>
+                            <span className='checkboxli checkbox-custom checkbox-default mr-2'>
                               <input
-                                type="checkbox"
-                                id="checkAll"
-                                name="checkbox"
-                                className=""
+                                type='checkbox'
+                                id='checkAll'
+                                name='checkbox'
+                                className=''
                                 checked={
                                   careGivers &&
                                   careGivers.getCaregivers &&
@@ -139,34 +148,32 @@ const BulkEmailCaregiver: FunctionComponent = () => {
                                   handleSelectAll();
                                 }}
                               />
-                              <label className=""></label>
+                              <label className=''></label>
                             </span>
                           </th>
-                          <th>{languageTranslation("NAME")}</th>
-                          <th>{languageTranslation("EMAIL")}</th>
+                          <th>{languageTranslation('NAME')}</th>
+                          <th>{languageTranslation('EMAIL')}</th>
                         </tr>
                       </thead>
                       <tbody>
                         {!called || loading ? (
                           <tr>
-                            <td className={"table-loader"} colSpan={8}>
+                            <td className={'table-loader'} colSpan={8}>
                               <Loader />
                             </td>
                           </tr>
-                        ) : careGivers &&
-                          careGivers.getCaregivers &&
-                          careGivers.getCaregivers.result.length ? (
-                          careGivers.getCaregivers.result.map(
+                        ) : careGiverData && careGiverData.length ? (
+                          careGiverData.map(
                             (careGivers: any, index: number) => {
                               return (
                                 <tr key={index}>
                                   <td>
-                                    <span className="checkboxli checkbox-custom checkbox-default mr-2">
+                                    <span className='checkboxli checkbox-custom checkbox-default mr-2'>
                                       <input
-                                        type="checkbox"
-                                        id="check"
-                                        name="checkbox"
-                                        className=""
+                                        type='checkbox'
+                                        id='check'
+                                        name='checkbox'
+                                        className=''
                                         checked={
                                           selectedCareGiver &&
                                           selectedCareGiver.length &&
@@ -184,7 +191,7 @@ const BulkEmailCaregiver: FunctionComponent = () => {
                                           );
                                         }}
                                       />
-                                      <label className=""></label>
+                                      <label className=''></label>
                                     </span>
                                   </td>
                                   <td>{`${careGivers.firstName} ${careGivers.lastName}`}</td>
@@ -194,80 +201,80 @@ const BulkEmailCaregiver: FunctionComponent = () => {
                             }
                           )
                         ) : (
-                          ""
+                          ''
                         )}
                       </tbody>
                     </Table>
                   </div>
                 </Col>
 
-                <Col lg={"7"}>
-                  <div className="">
-                    <div className="form-section py-2 px-3 bulk-email-form">
-                      <div className="d-flex align-items-end justify-content-between bulk-email-header">
-                        <Label className="bulk-email-label">
-                          {languageTranslation("SUBJECT")}{" "}
-                          {languageTranslation("EMAIL")}
+                <Col lg={'7'}>
+                  <div className=''>
+                    <div className='form-section py-2 px-3 bulk-email-form'>
+                      <div className='d-flex align-items-end justify-content-between bulk-email-header'>
+                        <Label className='bulk-email-label'>
+                          {languageTranslation('SUBJECT')}{' '}
+                          {languageTranslation('EMAIL')}
                         </Label>
-                        <div className="select-box mb-2">
+                        <div className='select-box mb-2'>
                           <Select
-                            placeholder={languageTranslation("SELECT_TEMPLATE")}
+                            placeholder={languageTranslation('SELECT_TEMPLATE')}
                             options={State}
-                            classNamePrefix="custom-inner-reactselect"
-                            className="custom-reactselect"
+                            classNamePrefix='custom-inner-reactselect'
+                            className='custom-reactselect'
                           />
                         </div>
                       </div>
                       <Row>
-                        <Col lg={"12"}>
+                        <Col lg={'12'}>
                           <FormGroup>
                             <div>
                               <Input
-                                type="text"
-                                placeholder={languageTranslation("SUBJECT")}
-                                name={"lastName"}
-                                className="width-common"
+                                type='text'
+                                placeholder={languageTranslation('SUBJECT')}
+                                name={'lastName'}
+                                className='width-common'
                               />
                             </div>
                           </FormGroup>
                         </Col>
-                        <Col lg={"12"}>
+                        <Col lg={'12'}>
                           <FormGroup>
-                            <Label className="form-label col-form-label mb-2">
-                              {languageTranslation("TEXT_EMAIL")}
+                            <Label className='form-label col-form-label mb-2'>
+                              {languageTranslation('TEXT_EMAIL')}
                             </Label>
 
                             <div>
                               <Editor
                                 // editorState={editorState}
-                                toolbarClassName="toolbarClassName"
-                                wrapperClassName="wrapperClassName"
-                                editorClassName="editorClassName"
-                                placeholder="Enter Email Content Here"
+                                toolbarClassName='toolbarClassName'
+                                wrapperClassName='wrapperClassName'
+                                editorClassName='editorClassName'
+                                placeholder='Enter Email Content Here'
                                 toolbar={{
                                   options: [
-                                    "inline",
-                                    "blockType",
-                                    "fontSize",
-                                    "list",
-                                    "textAlign",
-                                    "link"
+                                    'inline',
+                                    'blockType',
+                                    'fontSize',
+                                    'list',
+                                    'textAlign',
+                                    'link'
                                   ],
                                   inline: {
-                                    options: ["bold", "italic", "underline"]
+                                    options: ['bold', 'italic', 'underline']
                                   },
                                   fontSize: {
-                                    className: "bordered-option-classname"
+                                    className: 'bordered-option-classname'
                                   },
                                   fontFamily: {
-                                    className: "bordered-option-classname"
+                                    className: 'bordered-option-classname'
                                   },
                                   list: {
                                     inDropdown: false,
-                                    options: ["unordered"]
+                                    options: ['unordered']
                                   },
                                   link: {
-                                    options: ["link"]
+                                    options: ['link']
                                   }
                                 }}
                               />
@@ -276,30 +283,30 @@ const BulkEmailCaregiver: FunctionComponent = () => {
                         </Col>
                       </Row>
                     </div>
-                    <Table bordered hover responsive className="mail-table">
-                      <thead className="thead-bg">
+                    <Table bordered hover responsive className='mail-table'>
+                      <thead className='thead-bg'>
                         <tr>
-                          <th className="file-name">
-                            {languageTranslation("FILE_NAME")}
+                          <th className='file-name'>
+                            {languageTranslation('FILE_NAME')}
                           </th>
-                          <th className="size-col">
-                            {languageTranslation("SIZE")}
+                          <th className='size-col'>
+                            {languageTranslation('SIZE')}
                           </th>
                         </tr>
                       </thead>
                       <tbody>
                         <tr>
-                          <td className="file-name ">Pan Card.PDF</td>
-                          <td className="size-col">1kb</td>
+                          <td className='file-name '>Pan Card.PDF</td>
+                          <td className='size-col'>1kb</td>
                         </tr>
                         <tr>
-                          <td className="file-name">VoterID.pdf</td>
-                          <td className="size-col">2kb</td>
+                          <td className='file-name'>VoterID.pdf</td>
+                          <td className='size-col'>2kb</td>
                         </tr>
 
                         <tr>
-                          <td className="file-name">Pan Card.PDF</td>
-                          <td className="size-col">5kb</td>
+                          <td className='file-name'>Pan Card.PDF</td>
+                          <td className='size-col'>5kb</td>
                         </tr>
                       </tbody>
                     </Table>

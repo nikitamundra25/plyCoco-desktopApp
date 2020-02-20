466b23de167e3b7813f3de0fe07b70802f7733fb
rohit-chapter <rohit.chapter247@gmail.com>
2020-02-20 14:12:35 +0530

    loader & checkbox  issue fix

--- src/resource/views/pages/CareGiver/Signature/Signature.tsx
+++ src/resource/views/pages/CareGiver/Signature/Signature.tsx
@@ -1,52 +1,52 @@
-import React, { FunctionComponent } from 'react';
-import { FormGroup, Label, Col, Row, Button, Form } from 'reactstrap';
-import signature from '../../../../assets/img/sign.png';
+import React, { FunctionComponent } from "react";
+import { FormGroup, Label, Col, Row, Button, Form } from "reactstrap";
+import signature from "../../../../assets/img/sign.png";
 const Signature: FunctionComponent = () => {
   return (
     <>
-      <div className='signature-section'>
+      <div className="signature-section">
         <div>
-          <h5 className='content-title'>Current Signature</h5>
-          <div className='form-card'>
+          <h5 className="content-title">Current Signature</h5>
+          <div className="form-card">
             <div>
-              <img src={signature} alt='signature' />
+              <img src={signature} alt="signature" />
             </div>
           </div>
-          <Button color='danger' type='submit' size='sm' className='mb-3'>
-            <i className='fa fa-times mr-2'></i>Clear
+          <Button color="danger" type="submit" size="sm" className="mb-3">
+            <i className="fa fa-times mr-2"></i>Clear
           </Button>
         </div>
-        <Form className='form-section'>
+        <Form className="form-section">
           <Row>
-            <Col lg={'12'}>
-              <h5 className='main-title mb-4'>Upload Signature</h5>
-              <div className='form-card'>
+            <Col lg={"12"}>
+              <h5 className="main-title mb-4">Upload Signature</h5>
+              <div className="form-card">
                 <Row>
-                  <Col lg={'12'}>
+                  <Col lg={"12"}>
                     <FormGroup>
                       <Row>
-                        <Col sm='12'>
-                          <Label className='form-label col-form-label text-left'>
-                            Signature{' '}
+                        <Col sm="12">
+                          <Label className="form-label col-form-label text-left">
+                            Signature{" "}
                             <span>
                               (maximum 100kByte, only jpg, png or gif files)
                             </span>
                           </Label>
                         </Col>
-                        <Col sm='12'>
-                          <div className='custom-file-div'>
+                        <Col sm="12">
+                          <div className="custom-file-div">
                             <input
-                              type='file'
-                              id='FileBrowser'
-                              name='customFile'
-                              className='custom-input-file'
+                              type="file"
+                              id="FileBrowser"
+                              name="customFile"
+                              className="custom-input-file"
                             />
                             <Label
-                              className='custom-label-file'
-                              for='FileBrowser'
+                              className="custom-label-file"
+                              for="FileBrowser"
                             >
-                              <span className='choosefile-label'>
-                                <i className='fa fa-folder-open-o mr-2'></i>{' '}
+                              <span className="choosefile-label">
+                                <i className="fa fa-folder-open-o mr-2"></i>{" "}
                                 <span>Choose a file</span>
                               </span>
                             </Label>
@@ -58,16 +58,16 @@ const Signature: FunctionComponent = () => {
                 </Row>
               </div>
             </Col>
-            <Col lg={'12'}>
-              <div className='d-flex align-items-center justify-content-between'>
+            <Col lg={"12"}>
+              <div className="d-flex align-items-center justify-content-between">
                 <div>
                   <Button
-                    color='primary'
-                    type='submit'
-                    size='sm'
-                    className='mb-3 mr-3'
+                    color="primary"
+                    type="submit"
+                    size="sm"
+                    className="mb-3 mr-3"
                   >
-                    <i className='fa fa-upload mr-2'></i>Upload
+                    <i className="fa fa-upload mr-2"></i>Upload
                   </Button>
                 </div>
               </div>
@@ -75,21 +75,21 @@ const Signature: FunctionComponent = () => {
           </Row>
         </Form>
         <div>
-          <h5 className='content-title'>Sign here with mouse</h5>
-          <div className='form-card'>
-            <div className='bg-white py-5 w-100'></div>
+          <h5 className="content-title">Sign here with mouse</h5>
+          <div className="form-card">
+            <div className="bg-white py-5 w-100"></div>
           </div>
           <div>
             <Button
-              color='primary'
-              type='submit'
-              size='sm'
-              className='mb-3 mr-3'
+              color="primary"
+              type="submit"
+              size="sm"
+              className="mb-3 mr-3"
             >
-              <i className='fa fa-check mr-2'></i>Save Signature
+              <i className="fa fa-check mr-2"></i>Save Signature
             </Button>
-            <Button color='danger' type='submit' size='sm' className='mb-3'>
-              <i className='fa fa-times mr-2'></i>Empty Signature
+            <Button color="danger" type="submit" size="sm" className="mb-3">
+              <i className="fa fa-times mr-2"></i>Empty Signature
             </Button>
           </div>
         </div>

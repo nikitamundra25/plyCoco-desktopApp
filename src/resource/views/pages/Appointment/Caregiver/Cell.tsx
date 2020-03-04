01e68fe5cfe78b1b5338d066b248d0b41adfddc1
Rishabh Bula <rishab.chapter247@gmail.com>
2020-03-04 16:24:07 +0530

    major changes in appointment section

--- src/resource/views/pages/Appointment/Caregiver/Cell.tsx
+++ src/resource/views/pages/Appointment/Caregiver/Cell.tsx
@@ -1,6 +1,7 @@
 import { createSelectable } from 'react-selectable-fast';
 import React from 'react';
 import classnames from 'classnames';
+import moment from 'moment';
 const Cell = ({
   selectableRef,
   isSelected,
@@ -10,7 +11,6 @@ const Cell = ({
   handleSelectedUser
 }: any) => (
   <>
-    {/* {console.log(list.firstName === 'Aayushi' ? list : null, 'list')} */}
     <td
       className={classnames({
         'calender-col': true,
@@ -18,11 +18,45 @@ const Cell = ({
         'custom-appointment-col': true,
         'cursor-pointer': true,
         'selected-cell': isSelected,
-        'selecting-cell': isSelecting
+        'selecting-cell': isSelecting,
+        'cell-available': !isSelected
+          ? list &&
+            list.caregiver_avabilities &&
+            list.caregiver_avabilities.length
+            ? list.caregiver_avabilities.filter(
+                (avabilityData: any, index: number) => {
+                  return moment(day.isoString).format('DD.MM.YYYY') ===
+                    moment(avabilityData.date).format('DD.MM.YYYY') &&
+                    (avabilityData.f === 'available' ||
+                      avabilityData.s === 'available' ||
+                      avabilityData.n === 'available')
+                    ? true
+                    : false;
+                }
+              ).length
+              ? true
+              : false
+            : false
+          : false
       })}
       ref={selectableRef}
       // onClick={() => handleSelectedUser(list, day, 'caregiver')}
-    ></td>
+    >
+      {list && list.caregiver_avabilities && list.caregiver_avabilities.length
+        ? list.caregiver_avabilities.map(
+            (avabilityData: any, index: number) => {
+              return moment(day.isoString).format('DD.MM.YYYY') ===
+                moment(avabilityData.date).format('DD.MM.YYYY') ? (
+                <>
+                  {avabilityData.f === 'available' ? 'f' : null}
+                  {avabilityData.s === 'available' ? 's' : null}
+                  {avabilityData.n === 'available' ? 'n' : null}
+                </>
+              ) : null;
+            }
+          )
+        : null}
+    </td>
   </>
 );
 
--- src/resource/views/pages/Appointment/index.tsx
+++ src/resource/views/pages/Appointment/index.tsx
@@ -266,7 +266,6 @@ const Appointment: FunctionComponent = () => {
 
   useEffect(() => {
     console.log("careInstituionDept",careInstituionDept);
-    
   }, [careInstituionDept]);
 
   // select careGiver or careinstitution

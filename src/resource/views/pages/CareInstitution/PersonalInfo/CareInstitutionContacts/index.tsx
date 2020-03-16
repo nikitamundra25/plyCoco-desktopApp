9fb830c8bcb3760a62f9147c817b354ab76b8fdd
Nk Patil <nandkishor.chapter247@gmail.com>
2020-03-16 12:51:22 +0530

    Fixed Issue #412

--- src/resource/views/pages/CareInstitution/PersonalInfo/CareInstitutionContacts/index.tsx
+++ src/resource/views/pages/CareInstitution/PersonalInfo/CareInstitutionContacts/index.tsx
@@ -28,6 +28,7 @@ import CotactFormComponent from './CotactFormComponent';
 import { ConfirmBox } from '../../../../components/ConfirmBox';
 import Loader from '../../../../containers/Loader/Loader';
 import close from '../../../../../assets/img/close.svg';
+import { Gender } from '../../../../../../config';
 
 let toastId: any;
 
@@ -418,11 +419,10 @@ const CareInstitutionContacts: any = (props: any) => {
     title,
     contactType: selecContactType,
     gender: gender
-      ? {
-          label: gender,
-          value: gender,
-        }
-      : undefined,
+    ? Gender.filter(
+        ({ value }: IReactSelectInterface) => value === gender,
+      )[0]
+    : undefined,
     salutation: salutation
       ? {
           label: salutation,

6700a0e6ef7de0fa8c1f986dd8851332fc89112c
Rishabh Bula <rishab.chapter247@gmail.com>
2020-03-18 12:28:57 +0530

    small updationin infinite loading

--- src/resource/views/pages/Appointment/Caregiver/CaregiverListView.tsx
+++ src/resource/views/pages/Appointment/Caregiver/CaregiverListView.tsx
@@ -426,12 +426,17 @@ const CaregiverListView: FunctionComponent<IAppointmentCareGiverList> = (
           </NavItem> */}
         </Nav>
       </div>
+      {console.log('careGiversList.length', careGiversList.length)}
+      {console.log('totalCaregiver', totalCaregiver)}
       <div className='position-relative'>
         <InfiniteScroll
           loader={<div className='appointment-list-loader'>{''}</div>}
           hasMore={careGiversList && careGiversList.length !== totalCaregiver}
-          dataLength={10}
+          dataLength={
+            careGiversList && careGiversList.length ? careGiversList.length : 0
+          }
           next={() => {
+            console.log('In next contains');
             getNext(careGiversList.length);
           }}
           // endMessage={<p />}

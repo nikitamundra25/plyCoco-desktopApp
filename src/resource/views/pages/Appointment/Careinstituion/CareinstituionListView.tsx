aaa5b6080d78f903015136ccd46f43059691ce8b
nikita mundra <nikitam.chapter247@gmail.com>
2020-03-24 10:02:39 +0530

    highlight cells in careinstitution

--- src/resource/views/pages/Appointment/Careinstituion/CareinstituionListView.tsx
+++ src/resource/views/pages/Appointment/Careinstituion/CareinstituionListView.tsx
@@ -140,7 +140,7 @@ const CarinstituionListView: FunctionComponent<IAppointmentCareInstitutionList &
           caregiver,
           canstitution,
           dept: { id: deptId, name },
-          item: temp && temp.qualificationId && temp.qualificationId.length ? temp : item,
+          item: temp && temp.qualificationId && temp.qualificationId ? temp : item,
           qualificationIds: qualificationId,
           dateString: day ? day.dateString : '',
           divisions
@@ -416,6 +416,7 @@ const CarinstituionListView: FunctionComponent<IAppointmentCareInstitutionList &
                           : ''
                       }
                       handleSelectedAvailability
+                      selectedCells={selectedCells}
                     />
                   );
                 })}

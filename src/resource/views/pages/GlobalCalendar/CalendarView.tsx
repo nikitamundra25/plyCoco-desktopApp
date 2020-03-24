849b2ce5d10416562b207b7718c04cd8f99e392e
sohail <sohail.chapter247@gmail.com>
2020-03-24 19:03:18 +0530

    some changes

--- src/resource/views/pages/GlobalCalendar/CalendarView.tsx
+++ src/resource/views/pages/GlobalCalendar/CalendarView.tsx
@@ -158,7 +158,7 @@ const CalendarView: FunctionComponent<ICalendarViewProps> = ({
               </div>
             </div>
           </div>
-        </div>
+        </div>  
         
       </>
       <CardBody>

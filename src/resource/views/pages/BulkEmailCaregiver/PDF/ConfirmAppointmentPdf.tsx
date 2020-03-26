c5b2e762df0d2e71e99baf0503a00d26690f3b8a
Rohit-chapter <rohit.chapter247@gmail.com>
2020-03-26 20:06:06 +0530

    design pdf for termination & confirmation

--- src/resource/views/pages/BulkEmailCaregiver/PDF/ConfirmAppointmentPdf.tsx
+++ src/resource/views/pages/BulkEmailCaregiver/PDF/ConfirmAppointmentPdf.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Document, Page, Text, Image, View, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+import { Document, Page, Text, Image, View, StyleSheet, PDFViewer, Link } from '@react-pdf/renderer';
 import { IConfirmAppointmentPdfProps } from '../../../../../interfaces';
 
 const ConfirmAppointmentPdf: FunctionComponent<IConfirmAppointmentPdfProps> = (
@@ -16,9 +16,9 @@ const ConfirmAppointmentPdf: FunctionComponent<IConfirmAppointmentPdfProps> = (
     section: {
       margin: 10,
       padding: 10,
+      flexGrow: 1,
       marginBottom: '0',
       paddingBottom: '0',
-      flexGrow: 1
     },
     name: {
       fontSize: 24,
@@ -28,49 +28,47 @@ const ConfirmAppointmentPdf: FunctionComponent<IConfirmAppointmentPdfProps> = (
     subtitle: {
       fontSize: 15,
       fontWeight: 'bold',
-
       marginBottom: '15px'
     },
     subtext: {
-      fontSize: 12,
+      fontSize: 11,
       color: 'black',
       marginTop: '8px',
-      marginBottom: '8px'
+      marginBottom: '8px',
 
+    },
+    subtextbold: {
+      fontSize: 12,
+      color: 'black',
+      marginBottom: '10px',
+      paddingBottom: '4px',
+      fontWeight: 'bold',
+      borderBottomWidth: 2,
+      borderColor: 'black',
+      borderStyle: 'solid',
     },
     signaturecontainer: {
       flex: 1,
       flexDirection: 'row',
-      marginBottom: '0',
+      marginTop: '0px',
+      marginBottom: '0px',
       flexGrow: 1,
       marginLeft: '10px',
       marginRight: '10px',
     },
-    image: {
-      width: '50%',
+    imagediv: {
+      
       height: '50px',
-      padding: 10,
+      margin: 10,
     },
     imgtext: {
-      borderTopWidth: 1,
-      borderColor: 'black',
-      borderStyle: 'solid',
+      marginLeft: '10px',
+      marginRight: '10px',
       paddingTop: "10px",
-      fontSize: 12,
+      fontSize: 11,
       fontWeight: 'bold',
     },
-    textwrapper: {
-      margin: 10,
-      padding: 10,
-      marginBottom: '0',
-      paddingBottom: '0',
-      flexGrow: 1
-    },
-    remarktext: {
-      fontSize: 12,
-      color: 'black',
-      marginBottom: '4px'
-    },
+    
     footerwrapper: {
       margin: 10,
       textAlign: 'center',
@@ -93,43 +91,37 @@ const ConfirmAppointmentPdf: FunctionComponent<IConfirmAppointmentPdfProps> = (
         <Page size="A4" style={styles.page}>
           <View style={styles.section}>
             <Text style={styles.name}>TIMYOCY</Text>
-            <Text style={styles.subtitle}>Temporary working agreement  </Text>
-            <Text style={styles.subtext}>Between </Text>
-            <Text style={styles.subtext}>Diamond Personal GmbH, Welfenallee 3-7, 13465 Berlin under the label TIMyoCY </Text>
-            <Text style={styles.subtext}> - hereinafter: distributor – </Text>
-            <Text style={styles.subtext}>and </Text>
-            <Text style={styles.subtext}>…. </Text>
-            <Text style={styles.subtext}>- hereinafter: temporary agency workers – </Text>
-            <Text style={styles.subtext}>The employer was given a temporary permit to provide temporary workers on June 19, 2017 by the Federal Employment Agency, Employment Agency in Düsseldorf. With reference to the framework agreement of the parties agree the following use: </Text>
-            <Text style={styles.subtext}>24.-26.01.2020 FD, Place of work: Am Strengfeld, 14542 Werder, Seniorenwohnpark "Blütentraum", Haus 3, job: nursing assistant </Text>
-            <Text style={styles.subtext}>I would like to be employed indefinitely on identical terms: yes (     ) / no (    ) (please check). </Text>
+            <Link
+        style={styles.subtext}
+        src="#"
+      >
+        Diamond Personal GmbH, unter dem Label TIMyoCY · Welfenallee 3-7 · 13465 Berlin 
+      </Link>
+            <Text style={styles.subtext}>Arkadia Pflege Betriebsgesellschaft mbH  </Text>
+            <Text style={styles.subtext}>Senioren- und Pflegewohnpark Blütentraum  </Text>
+            <Text style={styles.subtext}>Senioren- und Pflegewohnpark Blütentraum </Text>
+            <Text style={styles.subtext}>14542 Werder  </Text>
+            
           </View>
+          <View style={styles.section}>
+          <Text style={styles.subtitle}>Supply of temporary workers  </Text>
+          <Text style={styles.subtext}>Supply of temporary workers according to the mutually agreed  leasing framework contract.   </Text></View>
+          <View style={styles.section}>
+          <Text style={styles.subtextbold}>No. Temporary work details   </Text>
+          <Text style={styles.subtext}>24.01.2020, FD, Worker: Jaroslaw Majewski, Qualification: Pflegehelfer, pay group: 3 </Text>
+          <Text style={styles.subtext}>25.01.2020, FD, Worker: Jaroslaw Majewski, Qualification: Pflegehelfer, pay group: 3 </Text>
+          <Text style={styles.subtext}>26.01.2020, FD, Worker: Jaroslaw Majewski, Qualification: Pflegehelfer, pay group: 3 </Text>
+          </View>
+          
           <View style={styles.signaturecontainer}>
-            <View style={styles.image}>
-              <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Richard_Feynman_signature.svg/1280px-Richard_Feynman_signature.svg.png" />
+            <View>
+              <Text style={styles.imagediv}>
+               
+              </Text>
               <Text style={styles.imgtext}>
-                Distributor
-          </Text>
-            </View>
-            <View style={styles.image}>
-              <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Richard_Feynman_signature.svg/1280px-Richard_Feynman_signature.svg.png" />
-              <Text style={styles.imgtext}>
-                Temporary Worker
-          </Text>
-            </View>
-          </View>
-          <View style={styles.textwrapper}>
-            <Text style={styles.remarktext}> With his further signature, the employee confirms that he identifies permanent part-time employment Conditions were offered, but he prefers this limited form of employment to a job in permanent employment. </Text>
-            <Text style={styles.remarktext}>He is aware that in this case there is no guarantee wage claim beyond the agreed working hours and, as a rule, also </Text>
-            <Text style={styles.remarktext}>there is no entitlement to the granting of vacation or continued payment of sickness. </Text>
-          </View>
-          <View style={styles.signaturecontainer}>
-            <View style={styles.image}>
-              <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Richard_Feynman_signature.svg/1280px-Richard_Feynman_signature.svg.png" />
-              <Text style={styles.imgtext}>
-                Temporary Worker
-          </Text>
-            </View>
+              Please sign it and send it back via mail. 
+              </Text>
+              </View>
           </View>
           <View style={styles.footerwrapper}>
             <Text style={styles.footertext}>TIMyoCY is a service of Diamond Personal GmbH · Welfenallee 3-7 · 13465 Berlin </Text>

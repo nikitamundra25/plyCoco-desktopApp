c1df98d10613f93d8f9ec2f704081bd15a81c3bc
bhumikabalchandani8 <bhumika.chapter247@gmail.com>
2020-04-09 12:23:01 +0530

    added reserved condition

--- src/resource/views/pages/Appointment/Caregiver/CaregiverListView.tsx
+++ src/resource/views/pages/Appointment/Caregiver/CaregiverListView.tsx
@@ -491,6 +491,18 @@ const CaregiverListView: FunctionComponent<IAppointmentCareGiverList> = (
       temp.push({ ...element, new: item, row });
     });
   });
+
+  //reserved condition
+  let reserveCondition: any;
+  if (selectedCells && selectedCells.length) {
+    reserveCondition = selectedCells.filter((x: any) => {
+      if (x.item) {
+        return x.item && x.item.status === 'default';
+      } else {
+        return ['abc'];
+      }
+    });
+  }
   return (
     <div>
       <div
@@ -524,7 +536,9 @@ const CaregiverListView: FunctionComponent<IAppointmentCareGiverList> = (
           </NavItem>
           <NavItem>
             <NavLink
-              disabled={selectedCells ? selectedCells.length === 0 : true}
+              disabled={
+                reserveCondition && reserveCondition.length === 0 ? true : false
+              }
               onClick={() => {
                 setopenToggleMenu(false);
                 onReserve ? onReserve() : undefined;

8ea66c025d76f4c3a8b411ad1caa53cfbb5565ef
aayushij <ayushij.chapter247@gmail.com>
2020-08-04 15:06:28 +0530

    changes in bulk email caregiver content

--- src/resource/views/pages/BulkEmailCaregiver/index.tsx
+++ src/resource/views/pages/BulkEmailCaregiver/index.tsx
@@ -208,7 +208,7 @@ const BulkEmailCaregiver: FunctionComponent<any> = (props: any) => {
   
   const setDefaultSignature = (body: any) => {  
     const contentBlock = htmlToDraft(
-      `<div>${body}<div><span style="font-size:15px; margin:0px 0px;">${languageTranslation(
+      `${body}<span style="font-size:15px; margin:0px 0px;">${languageTranslation(
         "BEST_WISHES"
       )}</span><br><span style="font-size:15px; margin:0px 0px;">${firstName} ${lastName}</span><br><span style="text-align:left;"><a href="https://www.plycoco.de/"><img alt="" src="${AppConfig.APP_ENDPOINT}/plycoco.png" style="height: auto; width: 180px; margin:0px;"></a></span></div><div><span><strong>Tel:</strong> <a href="tel:+49.30.644 99 444" style="color: #000; text-decoration: none;">+49.30.644 99 444</a></span><br><span><strong>Fax:</strong> <a href="fax: +49.30.644 99 445" style="color: #000; text-decoration: none;">+49.30.644 99 445</a></span><br><span><strong>E-Mail:</strong> <a href="mailto:kontakt@plycoco.de" style="color: #000; text-decoration: none;">kontakt@plycoco.de</a></span><br><span><a href="https://www.plycoco.de/" style="color: #000; text-decoration: none;">www.plycoco.de</a></span></div><div><span style="font-size: 12px;color: #b5b4b4;">Plycoco GmbH, Welfenallee 3-7, 13465 Berlin</span><br><span style="font-size: 12px;color: #b5b4b4;">Eintragung im Handelsregister: Registergericht: Amtsgericht Berlin-Charlottenburg, Registernummer: HRB 150746, Geschäftsführerin: Maren Krusch</span><br><span style="font-size: 12px;color: #b5b4b4;">Tel: +49.30.644 99 444, Fax: +49.30.644 99 445</span></div></div>`
     );
@@ -799,7 +799,7 @@ const BulkEmailCaregiver: FunctionComponent<any> = (props: any) => {
         }
 
         let divRow: string = '';
-        let customSub: string = 'Offer ';
+        let customSub: string = `${languageTranslation('OFFER')} `;
         divisionArray.map((v: any, i: number) => {
           if (v.id) {
             divRow += `<p>${v.date +

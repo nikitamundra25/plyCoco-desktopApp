fda2c9040b3eb3d65c8eeb1c95e259a797415c70
chapter247-yash <yash.chapter247@gmail.com>
2020-09-14 18:42:55 +0530

    getLeasingContractPDF changes

--- src/graphql/queries/LeasingContract.ts
+++ src/graphql/queries/LeasingContract.ts
@@ -1,12 +1,10 @@
 import gql from 'graphql-tag';
 
 const GET_LEASING_CONTRACT = gql`
-  query getLeasingContractPDF($userId: ID, $documentUploadType: String, $appointmentId: [ID]) {
-    getLeasingContractPDF(userId:$userId, documentUploadType: $documentUploadType, appointmentId:$appointmentId) {
-      document
-      leasingContract{
-				avabilityId
-      }
+  query getLeasingContractPDF($userId: ID, $appointmentId: [ID]) {
+    getLeasingContractPDF(userId:$userId, appointmentId:$appointmentId) {
+      id
+      attachment
     }
   }
 `;

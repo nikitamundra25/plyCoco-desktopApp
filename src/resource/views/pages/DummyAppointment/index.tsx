18c397234e4e3bbe67b0917d44b6cdda7d9a2abd
Sonu <sonu@chapter247.com>
2020-10-24 19:46:05 +0530

    imprt

--- src/resource/views/pages/DummyAppointment/index.tsx
+++ src/resource/views/pages/DummyAppointment/index.tsx
@@ -24,6 +24,7 @@ import {
   IReactSelectInterface,
   IAddCargiverAppointmentRes,
   IStarInterface,
+  ICareinstitutionFormSubmitValue,
 } from "../../../../interfaces";
 import {
   germanNumberFormat,

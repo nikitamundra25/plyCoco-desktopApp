261baef25b75e93a5f8fc601d725d31e7db8d9c3
Sonu <sonu@chapter247.com>
2020-10-26 11:55:10 +0530

    Minor updates

--- src/resource/views/pages/Temp/index.tsx
+++ src/resource/views/pages/Temp/index.tsx
@@ -14,7 +14,7 @@ import { AppointmentsQueries } from "../../../../graphql/queries";
 import { getDaysArrayByMonth } from "../../../../helpers";
 const [GET_USERS_BY_QUALIFICATION_ID] = AppointmentsQueries;
 const staticHeader = ["caregiver", "H", "S", "U", "V"];
-
+let allCaregivers: any[] = [];
 export const TempPage = () => {
   const [daysData, setDaysData] = useState(
     getDaysArrayByMonth(moment().month(), moment().year())
@@ -52,6 +52,9 @@ export const TempPage = () => {
       },
     });
   }, []);
+  /**
+   *
+   */
   const setCaregivers = () => {
     const data =
       ((careGiversList || {}).getUserByQualifications || {}).result || [];
@@ -82,21 +85,37 @@ export const TempPage = () => {
       }
     });
     console.timeEnd("test");
+    allCaregivers = Object.assign([], newData);
     setCaregiverData(newData);
   };
   useEffect(() => {
+    console.log("fasdf");
     if (!loadingCaregiver) {
       setCaregivers();
     }
   }, [loadingCaregiver]);
 
   const columns = [...staticHeader, ...daysData.daysArr];
+  /**
+   *
+   * @param index
+   */
   const onAddNewRow = (index: number) => {
-    console.log(caregivers);
-    const newCaregivers = Object.assign([], caregivers);
-    newCaregivers.splice(index, 0, newCaregivers[index]);
-    console.log(newCaregivers);
-    // setCaregiverData(newCaregivers);
+    console.log(allCaregivers);
+    const newCaregivers = Object.assign([], allCaregivers);
+    newCaregivers.splice(index + 1, 0, {
+      ...allCaregivers[index],
+      firstName: "",
+      lastName: "",
+      key: allCaregivers[index].key + allCaregivers.length,
+    });
+    allCaregivers = newCaregivers;
+    setCaregiverData(newCaregivers);
+  };
+  const handleEndReached = (arg: any) => {
+    if (!loadingCaregiver) {
+      console.log(arg);
+    }
   };
   console.log(caregivers);
   return (
@@ -107,6 +126,33 @@ export const TempPage = () => {
         width={800}
         height={300}
         rowKey='key'
+        overlayRenderer={() =>
+          loadingCaregiver ? (
+            <>
+              <div
+                style={{
+                  pointerEvents: "none",
+                  background: "rgba(32, 60, 94, 0.3)",
+                  position: "absolute",
+                  bottom: "30px",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  padding: "5px 15px",
+                  borderRadius: "10px",
+                  display: "flex",
+                  alignItems: "center",
+                }}>
+                <span
+                  style={{
+                    color: "#fff",
+                    marginRight: "5px",
+                  }}>
+                  Loading...
+                </span>
+              </div>
+            </>
+          ) : null
+        }
         headerRenderer={() =>
           columns.map((d: any, index: number) =>
             staticHeader.indexOf(d) > -1 ? (
@@ -141,6 +187,8 @@ export const TempPage = () => {
             {cells}
           </div>
         )}
+        onEndReachedThreshold={300}
+        onEndReached={handleEndReached}
         headerClassName='custom-appointment-row'
         rowClassName='custom-appointment-row'>
         {columns.map((d: any, index: number) => (
@@ -177,7 +225,7 @@ export const TempPage = () => {
                   return (
                     <span
                       key={rowIndex}
-                      className='custom-appointment-col s-col text-center'>
+                      className='custom-appointment-col s-col text-center cursor-pointer'>
                       <i className='fa fa-star-o' />
                     </span>
                   );
@@ -185,7 +233,7 @@ export const TempPage = () => {
                   return (
                     <span
                       key={rowIndex}
-                      className='custom-appointment-col u-col text-center'>
+                      className='custom-appointment-col u-col text-center cursor-pointer'>
                       <i className='fa fa-star-o' />
                     </span>
                   );
@@ -193,7 +241,7 @@ export const TempPage = () => {
                   return (
                     <span
                       key={rowIndex}
-                      className='custom-appointment-col v-col text-center'
+                      className='custom-appointment-col v-col text-center cursor-pointer'
                       onClick={() => onAddNewRow(rowIndex)}>
                       <i className='fa fa-arrow-down' />
                     </span>
